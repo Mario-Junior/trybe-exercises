b081f789de182547f27c00792c9559bf93ecc6c6
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-11-06 10:27:38 -0300

    exercises-5-6 upload

---
+++ m1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/exercise-5.js
@@ -0,0 +1,22 @@
+console.log((2 + 2) === 4);
+console.log('\n');
+
+console.log(!(2 + 2) === 4);
+console.log('\n');
+
+const squirtle = "melhor pokemon inicial";
+
+console.log(!squirtle); // false
+console.log('\n');
+
+console.log(!42); // false
+console.log('\n');
+
+console.log(!0); // true
+// O número 0 tem o valor "falsy" no javascript. Logo, seu oposto é true.
+console.log('\n');
+
+console.log(!null); // true
+console.log('\n');
+
+console.log(!undefined); // true
