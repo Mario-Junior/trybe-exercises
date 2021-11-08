10fd36c57776ca2416b30233fadd8c32e32ff772
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-11-08 08:42:37 -0300

    practice-exercises resolution update

--- m1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/practice-exercises.js
+++ m1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/practice-exercises.js
@@ -13,8 +13,105 @@ for (let i = 0; i < numbers.length; i +=1) {
 console.log('\n');
 
 /* 2. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado; */
-let arrSum;
+
+let arrSum = 0;
+
 for (let i = 0; i < numbers.length; i +=1) {
-  arrSum = numbers[i] + numbers[i+1];
+  arrSum = arrSum + numbers[i];
 }
 console.log(arrSum);
+
+console.log('\n');
+
+/* 3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos. */
+
+let arrSum3 = 0;
+
+for (let i = 0; i < numbers.length; i +=1) {
+  /* arrSum3 = arrSum3 + numbers[i]; ou */
+  arrSum3 += numbers[i];
+}
+
+let arrAverage = arrSum3 / numbers.length;
+
+console.log(arrAverage);
+
+console.log('\n');
+
+/* 4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20"; */
+
+if (arrAverage > 20) {
+  console.log('valor maior que 20');
+} else {
+  console.log('valor menor ou igual a 20');
+}
+
+console.log('\n');
+
+/* 5. Utilizando for , descubra qual o maior valor contido no array e imprima-o; */
+
+let hValue = 0;
+
+for (let i = 0; i < numbers.length; i +=1) {
+  if (numbers[i] > hValue) {
+    hValue = numbers[i];
+  } else {
+  }
+}
+console.log(hValue);
+
+console.log('\n');
+
+/* 6.Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado"; */
+let odd = 0;
+
+for (let i = 0; i < numbers.length; i +=1) {
+  if (numbers[i] % 2 !== 0) {
+    odd += 1
+  } else {
+  }
+}
+if (odd === 0) {
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log(odd);
+}
+console.log('\n');
+
+/* 7.Utilizando for , descubra qual o menor valor contido no array e imprima-o; */
+
+let lowValue = 0;
+
+for (let i = 0; i < numbers.length; i +=1) {
+  if (lowValue === 0) {
+    lowValue = numbers[i];
+  } else if (numbers[i] < lowValue) {
+    lowValue = numbers[i];
+}
+}
+console.log(lowValue);
+
+console.log('\n');
+
+/* GABARITO (solução melhor logicamente que a minha - mas acrescentei '0' ao Array e minha solução também passou no teste ;D):
+o valor da variável smallestNumber poderia ser qualquer um, desde que fosse maior que o maior número do array numbers. Caso atribuíssemos o valor 1 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número menor que 1 no array. Para resolver esse problema vamos iniciar a variavel com o primeiro valor do array. Dessa forma podemos pular a posição 0 e iniciar com index = 1 */
+
+let smallestNumber = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] < smallestNumber) {
+    smallestNumber = numbers[index];
+  }
+}
+
+console.log(smallestNumber);
+console.log('\n');
+
+/* 8.Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado; */
+
+
+/* 9.Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 . */
+
+
+/* BÔNUS */
