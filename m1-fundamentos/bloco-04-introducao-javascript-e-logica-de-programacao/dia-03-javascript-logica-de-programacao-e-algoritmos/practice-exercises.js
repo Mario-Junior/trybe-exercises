14b192731b48237c5f70d4906e38450aa53cd23e
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-11-09 08:32:04 -0300

    exercises update

--- m1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/practice-exercises.js
+++ m1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/practice-exercises.js
@@ -8,13 +8,45 @@ n = 5
 *****
  */
 
-let n = 6;
-let asterisk = '*';
+let n = 5;
+let item = '*';
 let line = '';
 
 for (let i = 0; i < n; i += 1) {
-  line = line + asterisk;
+  line = line + item;
 }
 for (let i = 0; i < n; i += 1) {
   console.log(line);
+}
+console.log('\n');
+
+/* 2.Imprimir um triângulo retângulo com 5 asteriscos de base:
+n = 5
+*
+**
+***
+****
+***** */
+
+let item2 = '*'
+let column = '';
+for (let i = 0; i < n; i += 1) {
+  column = column + item2;
+  console.log(column);
+}
+console.log('\n');
+
+/* 3.Inverter o lado do triângulo - Atenção! Exercício é mais complexo que o anterior! É prciso uma lógica para imprimir os espaços:
+n = 5
+    *
+   **
+  ***
+ ****
+*****/
+let spaces = '-';
+let item3 = '*'
+let line3 = '';
+for (let i = 0; i < n; i += 1) {
+  line3 = line3 + spaces;
+  console.log(line3);
 }
