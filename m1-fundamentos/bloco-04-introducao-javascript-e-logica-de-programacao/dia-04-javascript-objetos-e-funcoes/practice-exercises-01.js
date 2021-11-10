ea6db8e981a9493c79a2b866f8ae4b66d96faa43
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-11-10 18:45:33 -0300

    exercises update

--- m1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/practice-exercises-01.js
+++ m1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/practice-exercises-01.js
@@ -15,8 +15,9 @@ function div(a, b) {
 function mod(a, b) {
   return a % b;
 };
-console.log(sum(10, 2), ' | ', subt(10, 2), ' | ', mult(10, 2), ' | ', div(10, 2), ' | ', mod(10, 2));
-/* 2. Faça um programa que retorne o maior de dois números. */
+console.log(sum(10, 2), ' | ', subt(14, 2), ' | ', mult(6, 2), ' | ', div(24, 2), ' | ', mod(1212, 12));
+
+// 2. Faça um programa que retorne o maior de dois números.
 function majorNum(a, b) {
   if (a > b) {
     return a + ' é maior que ' + b;
@@ -28,70 +29,42 @@ function majorNum(a, b) {
 };
 console.log(majorNum(12, 12));
 
-/* 3. Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados. */
-const a = 28;
-const b = 12;
-const c = 26;
-
-console.log('const a = ' + a);
-console.log('const b = ' + b);
-console.log('const b = ' + c);
-
-if (a > b && a > c) {
-  console.log('a é a maior');
-} else if (b > a && b > c) {
-  console.log('b é a maior');
-} else {
-  console.log('c é a maior');
-}
-
-/* 4. Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário. */
-
-const value = 12;
-
-console.log('const value = ' + value);
-
-if (value > 0) {
-  console.log('positive');
-} else if (value < 0) {
-  console.log('negative');
-} else {
-  console.log('zero');
-}
-
-/* 5. Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
-Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
-Um ângulo será considerado inválido se não tiver um valor positivo. */
-
-const angleA = 90;
-const angleB = 60;
-const angleC = 30;
-
-if (angleA <= 0 || angleB <=0 || angleC <=0) {
-  console.log('ATENÇÃO! Algum ângulo é inválido!');
-} else if (angleA + angleB + angleC === 180) {
-  console.log(true);
-} else {
-  console.log(false);
-}
-
-
-/* GABARITO: */
-
-let degreeAngleA = 90;
-let degreeAngleB = 60;
-let degreeAngleC = 30;
-
-let sumOfAngles = degreeAngleA + degreeAngleB + degreeAngleC;
-
-let allAnglesArePositives = degreeAngleA > 0 && degreeAngleB > 0 && degreeAngleC > 0;
-
-if(allAnglesArePositives){
-  if (sumOfAngles === 180) {
-    console.log(true);
+// 3. Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+function compareNumbers(a, b, c) {
+  if (a > b && a > c) {
+    return a + ' é o maior';
+  } else if (b > a && b > c) {
+    return b + ' é o maior';
+  } else if (c > a && c > b) {
+    return c + ' é o maior';
   } else {
-    console.log(false);
-  };
-} else {
-  console.log('Erro: ângulo inválido');
+    return 'Todos os números são iguais!';
+  }
+};
+console.log(compareNumbers(12, 12, 12));
+
+// 4. Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+function polarity(number) {
+  if (number > 0) {
+    return 'positive';
+  } else if (number < 0) {
+    return 'negative';
+  } else {
+    return 'zero';
+  }
+};
+console.log(polarity(-12));
+
+// 5. Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+// Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+// Um ângulo será considerado inválido se não tiver um valor positivo.
+function itsATriangle(a, b, c) {
+  if (a <= 0 || b <=0 || c <=0) {
+    return 'Attention, some angle is invalid!';
+  } else if (a + b + c === 180) {
+    return true;
+  } else {
+    return false;
+  }
 }
+console.log(itsATriangle(12, 120, 48));
---
+++ m1-fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/practice-exercises-02.js
@@ -0,0 +1,7 @@
+// Parte I - Objetos e For/In
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+// 1 - Imprimir mensagem de boas-vindas para a personagem acima, incluindo seu nome (Bem-vinda, Margarida)
