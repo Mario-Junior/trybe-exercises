b10a5ca4f368cb92e58d2e1897a1779f9c6b3cf1
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-11-14 17:39:47 -0300

    learning exercises update

--- m1-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/learning-exercises/script.js
+++ m1-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/learning-exercises/script.js
@@ -1,2 +1,16 @@
 /* Consolidar a manipulação dos elementos HTML , colocando cor no Administrador de Tempo da Trybe como na imagem, usando apenas o JavaScript! */
 
+document.getElementsByTagName('h1')[0].style.backgroundColor = 'MediumSeaGreen';
+document.getElementsByTagName('h1')[0].style.height = '60px';
+document.getElementsByTagName('h1')[0].style.paddingTop = '20px';
+
+document.querySelector('.emergency-tasks').style.backgroundColor = 'LightSalmon';
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'Yellow';
+
+document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = 'MediumOrchid';
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = 'MediumOrchid';
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = 'Black';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = 'Black';
+
+document.getElementById('footer-container').style.backgroundColor = 'DarkSlateGrey';
+document.getElementById('footer-container').style.marginInline = '20px';
