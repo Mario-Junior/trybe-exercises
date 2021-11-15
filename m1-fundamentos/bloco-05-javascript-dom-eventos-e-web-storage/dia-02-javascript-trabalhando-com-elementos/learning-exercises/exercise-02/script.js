61a4b1265420e42a1964d650f9478a9c59cf5267
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-11-15 20:45:53 -0300

    block-05-day-02 learning exercises update

--- m1-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/learning-exercises/exercise-02/script.js
+++ m1-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/learning-exercises/exercise-02/script.js
@@ -1,6 +1,7 @@
 // arquivo script.js
 // 1. Acessar elemento elementoOndeVoceEsta
 let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+elementoOndeVoceEsta.style.backgroundColor = 'PaleGreen';
 
 // 2. Acessar pai a partir de elementoOndeVoceEsta e adicionar uma color a ele
 let pai = elementoOndeVoceEsta.parentElement;
@@ -12,15 +13,20 @@ primeiroFilhoDoFilho.innerText = 'Texto adicionado com sucesso ao referido eleme
 
 // 4. Acesse o primeiroFilho a partir de pai
 let primeiroFilho = pai.firstElementChild;
+primeiroFilho.innerText = 'Número 4 feito com sucesso!';
 
 // 5.Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta
 let primeiroFilho2 = elementoOndeVoceEsta.previousElementSibling;
+primeiroFilho2.style.backgroundColor = 'yellow';
 
 // 6. Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta 
-let text = elementoOndeVoceEsta.nextSibling;
+let textElement = elementoOndeVoceEsta.nextSibling;
+textElement.innerText = 'Texto novo de teste'; //Porque não funciona???
 
 // 7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta
 let terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
+terceiroFilho.innerText = 'Número 7 feito com sucesso!';
 
 // 8. Agora acesse o terceiroFilho a partir de pai
 let terceiroFilho2 = pai.lastElementChild.previousElementSibling;
+terceiroFilho2.style.color = 'DeepPink';
