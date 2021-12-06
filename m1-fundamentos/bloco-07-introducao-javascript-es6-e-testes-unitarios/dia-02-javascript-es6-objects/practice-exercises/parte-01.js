e99814b3aae69070ad6d785bcd98467886121785
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-12-06 13:55:14 -0300

    7.2 practice-exercises update

--- m1-fundamentos/bloco-07-introducao-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/practice-exercises/parte-01.js
+++ m1-fundamentos/bloco-07-introducao-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/practice-exercises/parte-01.js
@@ -35,15 +35,21 @@ const order = {
 };
 
 const customerInfo = (order) => {
-  // Adicione abaixo as informações necessárias.
+  const deliveryPerson = order.order.delivery.deliveryPerson;
+  const orderName = order.name;
+  const orderPhone = order.phoneNumber;
+  const orderAddress = `${order.address.street} Nº: ${order.address.number}, Comp.: apto ${order.address.apartment}`;
 
+  console.log(`Olá ${deliveryPerson}; entrega para: ${orderName}; Telefone: ${orderPhone}; Endereço: ${orderAddress}.`);
 }
-
 customerInfo(order);
 
 const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
+  const newOrder = order.name = 'Luiz Silva';
+  const pizzas = Object.keys(order.order.pizza);
+  const drinks = order.order.drinks.coke.type;
+  const newTotal = order.payment.total = '50';
 
+  console.log(`Olá ${newOrder}; o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${drinks} é R$ ${newTotal},00.`);
 }
-
 orderModifier(order);
