a36b596324c4deeafb4cd31385cc834b1efbaf95
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-12-05 23:00:42 -0300

    7.2-learning-exercises update

---
+++ m1-fundamentos/bloco-07-introducao-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-objects/learning-exercises/parte-05.js
@@ -0,0 +1,76 @@
+// Parte IV - Object.assign
+const person = {
+  name: 'Alberto',
+  lastName: 'Gomes',
+  age: 20,
+};
+
+const info = {
+  age: 23,
+  job: 'engenheiro',
+};
+
+const family = {
+  children: ['Maria', 'João'],
+  wife: 'Ana',
+};
+
+Object.assign(person, info, family)
+console.log(person)
+
+/* Output
+  { name: 'Alberto',
+  lastName: 'Gomes',
+  age: 23,
+  job: 'engenheiro',
+  children: [ 'Maria', 'João' ],
+  wife: 'Ana'
+} */
+
+const person = {
+  name: 'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const clone = Object.assign(person, lastName);
+
+console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
+console.log(person); // { name: 'Roberto', lastName: 'Silva' }
+
+
+clone.name = 'Maria';
+
+console.log('Mudando a propriedade name através do objeto clone')
+console.log(clone); // Output: { name: 'Maria', lastName: 'Silva' }
+console.log(person); // Output: { name: 'Maria', lastName: 'Silva' }
+console.log('--------------');
+
+person.lastName = 'Ferreira';
+
+console.log('Mudando a propriedade lastName através do objeto person');
+console.log(clone); // Output: { name: 'Maria', lastName: 'Ferreira' }
+console.log(person); // Output: { name: 'Maria', lastName: 'Ferreira' }
+
+// Outra forma de modificar um objeto via clone:
+const obj = { value1: 10, value2: 11 };
+const cloneObj = obj;
+
+console.log(obj);
+console.log(cloneObj);
+
+// Resolvendo os problemas dos clones com objeto vazio {}
+const person = {
+  name:'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const newPerson = Object.assign({},person,lastName);
+newPerson.name = 'Gilberto';
+console.log(newPerson);
+console.log(person);
