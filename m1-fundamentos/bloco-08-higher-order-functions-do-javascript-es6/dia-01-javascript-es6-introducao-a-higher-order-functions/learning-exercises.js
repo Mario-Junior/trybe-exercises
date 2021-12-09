34a485e8b6dd03d63e3dff15e1f26cc710e99ee5
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-12-09 08:51:15 -0300

    08:01 learning-exercises update

--- m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/learning-exercises.js
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/learning-exercises.js
@@ -33,10 +33,70 @@ console.log(initialSum(5));
 // Função que simula o registro de uma pessoa e passamos como argumento de uma 2a função
 // Logo, addEventListener é uma HOF
 // Obs.: essa função não 'funciona', pois não há vínculo do arquivo HTML (document)
-const button = document.querySelector('#signup-button');
 
-const registerUser = () => {
-  console.log('Registrado com sucesso!');
+// const button = document.querySelector('#signup-button');
+
+// const registerUser = () => {
+//   console.log('Registrado com sucesso!');
+// };
+
+// button.addEventListener('click', registerUser);
+
+// Estruturando uma HOF
+const repeat = (number, action) => {
+  for (let count = 0; count <= number; count += 1) {
+    action(count);
+  }
+};
+repeat(5, console.log);
+
+// aumento de complexidade e construção de funções mais especializadas e bem definidas
+const repeat2 = (number, action) => {
+  for (let count = 0; count <= number; count += 1) {
+    action(count);
+  }
 };
 
-button.addEventListener('click', registerUser);
+repeat2(3, (number) => {
+  if (number % 2 === 0) {
+    console.log(number, 'is even');
+  }
+});
+
+// Testar quais números são ímpares. Veja como fica fácil ajustar a implementação
+const repeat3 = (number, action) => {
+  for (let count = 0; count <= number; count += 1) {
+    action(count);
+  }
+};
+
+const isEven = (number) => {
+  if (number % 2 === 0) {
+    console.log(number, 'is even');
+  }
+};
+
+const isOdd = (number) => {
+  if ((number % 2) > 0) {
+    console.log(number, 'is odd');
+  }
+};
+
+repeat3(3, isEven); // Testa quais números serão pares;
+repeat3(3, isOdd); // Testa quais números serão ímpares;
+
+const numberGenerator = () => {
+  return Math.random() * 100;
+}
+
+console.log(numberGenerator);
+// Não gera o número aleatório, pois imprimimos apenas a escrita da função
+
+// Versus
+
+const numberGenerator2 = () => {
+  return Math.random() * 100;
+}
+
+console.log(numberGenerator2());
+// Para executar a função, temos que inserir os () na frente dela
