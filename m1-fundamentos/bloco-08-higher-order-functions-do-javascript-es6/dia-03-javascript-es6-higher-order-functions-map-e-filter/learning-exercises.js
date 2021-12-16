8a357663ad6d8cc952399483044b0636a1a4a7e4
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-12-16 15:55:16 -0300

    08:03 learning-exercises update

--- m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/learning-exercises.js
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/learning-exercises.js
@@ -28,3 +28,14 @@ const verifyAgeDrive = (arrayOfPeople) => (
 
 console.log(verifyAgeDrive(objPeople));
 // [ { name: 'Maria', age: 16 }, { name: 'Vitor', age: 15 } ]
+
+
+// Outra forma de se usar filter é retornar um array sem o elemento desejado:
+const arrayMyStudents = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const removeStudentByName = (name, listStudents) =>
+  listStudents.filter((student) => student !== name);
+  // Filtra todos os estudantes que não têm o nome 'Ricardo' e retorna um array com eles. Na prática, remove o Ricardo do array.
+
+const newListStudents = removeStudentByName('Ricardo', arrayMyStudents);
+console.log(newListStudents); // [ 'Maria', 'Manuela', 'Jorge', 'Wilson' ]
