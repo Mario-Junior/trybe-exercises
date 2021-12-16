0a65390d54604049c0d2325d00d689cb2716dd8b
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-12-16 17:26:47 -0300

    08:03 learning-exercises update

--- m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/learning-exercises.js
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/learning-exercises.js
@@ -84,3 +84,16 @@ const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : 
 
 console.log('Negativos:', negativeNumbers); // [ -1, -2, -3, -4, -5 ]
 console.log('Array base:', numbers); // [ 1, 2, 3, 4, -5 ]
+
+// E o exemplo acima apenas com for:
+const negativeNumbers2 = [];
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] > 0) {
+    negativeNumbers2.push(numbers[index] * -1);
+  } else {
+    negativeNumbers2.push(numbers[index]);
+  }
+}
+
+console.log(negativeNumbers2); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
