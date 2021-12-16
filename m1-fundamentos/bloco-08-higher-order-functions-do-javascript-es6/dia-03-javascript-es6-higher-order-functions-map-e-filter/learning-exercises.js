79d195e54e2ac23a9c3ccdec130aeabfecf802b7
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-12-16 17:32:14 -0300

    08:03 learning-exercises update

--- m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/learning-exercises.js
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/learning-exercises.js
@@ -94,6 +94,22 @@ for (let index = 0; index < numbers.length; index += 1) {
     negativeNumbers2.push(numbers[index]);
   }
 }
-
 console.log(negativeNumbers2); // [ -1, -2, -3, -4, -5 ]
 console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+// Outras formas de usar o .map é unir dois arrays para criar um novo. Considere que você possui duas listas: o preço do primeiro produto ( Arroz ) é o primeiro elemento da lista prices ( 2.99 ), e assim por diante:
+const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+const prices = [2.99, 3.99, 1.5, 2];
+
+const detailedProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = detailedProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
