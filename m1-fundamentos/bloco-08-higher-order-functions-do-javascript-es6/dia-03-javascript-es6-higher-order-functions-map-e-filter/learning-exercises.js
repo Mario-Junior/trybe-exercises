438ef1358c736ee7527770aa123dc3a8bf7fa20f
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-12-16 15:50:08 -0300

    08:03 learning-exercises update

--- m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/learning-exercises.js
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/learning-exercises.js
@@ -12,3 +12,19 @@ console.log(isEven); // [ 30, 22 ]
 const isEven2 = numbers.filter((number) => number % 2 === 0);
 
 console.log(isEven2); // [ 30, 22 ]
+
+// Neste caso, deseja-se apenas as pessoas que não possuem ainda idade para dirigir:
+const objPeople = [
+  { name: 'José', age: 21 },
+  { name: 'Lucas', age: 19 },
+  { name: 'Maria', age: 16 },
+  { name: 'Gilberto', age: 18 },
+  { name: 'Vitor', age: 15 },
+];
+
+const verifyAgeDrive = (arrayOfPeople) => (
+  arrayOfPeople.filter((people) => (people.age < 18))
+);
+
+console.log(verifyAgeDrive(objPeople));
+// [ { name: 'Maria', age: 16 }, { name: 'Vitor', age: 15 } ]
