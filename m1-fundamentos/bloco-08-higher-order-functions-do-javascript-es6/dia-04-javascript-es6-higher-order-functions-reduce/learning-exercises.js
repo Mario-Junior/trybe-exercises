cc8170ec9ba90ab3547670d9d63c1ad7ec02a7d0
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-12-22 19:08:28 -0300

    8.4 learning-exercises update

--- m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/learning-exercises.js
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/learning-exercises.js
@@ -1,10 +1,12 @@
-// função soma todos os valores de um array:
-// Apenas com o for :
+// 8.3 array.reduce
+// Sintaxe: array.reduce(callback(acc, cur [, index [, array]]) [, init]);
+
+// Função soma todos os valores de um array:
+// Apenas com for :
 
 const numbers = [32, 15, 3, 2, -5, 56, 10];
 let sumNumbers = 0;
-// A variável 'acumula', a cada iteração do for, o resultado da operação feita lá!
-
+// A variável 'acumula', a cada iteração do for, o resultado da operação feita
 for (let index = 0; index < numbers.length; index += 1) {
   sumNumbers += numbers[index];
 }
@@ -20,6 +22,7 @@ const getSum = (result, number) => result + number;
 const sumNumbers3 = numbers.reduce(getSum);
 console.log(sumNumbers3); // 113
 
+// Outro exemplo:
 const collection = [1, 2, 3, 4, 5];
 
 const getSum2 = (accumulator, number) => {
@@ -29,3 +32,37 @@ const getSum2 = (accumulator, number) => {
 
 const sumNumbers4 = collection.reduce(getSum2);
 console.log(sumNumbers4); // 15
+
+// reduce também pode ser acionada com a utilização de um valor inicial como parâmetro `init`
+// Nota: Se o valorInicial não tiver sido passado como argumento, então reduce() executará o callback da função começando a partir do índice 1 (index 1), pulando o primeiro índice (index 0). Se o valorInicial for passado como argumento, a função irá começar no index 0 - https://mzl.la/3plxvQr
+
+// Sem alteração:
+const numbers2 = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum3 = (result, number) => {
+  console.log(result); // 32 47 50 52 47 103
+  return result + number;
+};
+
+const sumNumbers5 = numbers2.reduce(getSum3);
+console.log(sumNumbers5); // 113
+
+// Com alteração:
+const numbers3 = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum4 = (result, number) => {
+  console.log(result); // 0 32 47 50 52 47 103
+  return result + number;
+  };
+const sumNumbers6 = numbers3.reduce(getSum4, 0); // Parâmetro adicionado ao reduce: o "0" note no log anterior que a iteração começou dele;
+console.log(sumNumbers6); // 113
+
+// Alterando o init para outro valor para ver a diferença no resultado:
+const numbers4 = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum5 = (result, number) => {
+  console.log(result); // 10 42 57 60 62 57 113
+  return result + number;
+  };
+const sumNumbers7 = numbers4.reduce(getSum5, 10); // Parâmetro adicionado ao reduce: o 10;
+console.log(sumNumbers7); // 123
