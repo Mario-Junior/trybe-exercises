69f0bd3a5d959949d7f9ad557c89d497fb13137b
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-12-30 20:43:35 -0300

    8.4 practice-exercises 05 update

--- m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/practice-exercises/exercise05.js
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/practice-exercises/exercise05.js
@@ -6,5 +6,19 @@ const names = [
 ];
 
 function containsA() {
-  // escreva seu código aqui
+  let a = 0;
+  const phrase = names.reduce((acc, curr) => acc + curr);
+  for (let i = 0; i < phrase.length; i += 1) { phrase[i] === 'A' || phrase[i] === 'a' ? a += 1 : a };
+  return a;
 }
+console.log(containsA());
+
+// Gabarito:
+function containsA() {
+  return names.reduce((acc, curr) =>
+     acc + curr.split('').reduce((acumulator, current) => {
+        if (current === 'a' || current === 'A') return acumulator + 1;
+        return acumulator;
+     }, 0), 0);
+}
+console.log(containsA());
