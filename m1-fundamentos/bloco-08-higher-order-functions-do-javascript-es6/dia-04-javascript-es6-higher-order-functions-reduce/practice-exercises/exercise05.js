7363c47d50377103869d5a903f2653b02378d49e
Mario-Junior <mariojuniorprofissional@gmail.com>
2021-12-30 20:30:09 -0300

    8.4 practice-exercises 05 update

---
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/practice-exercises/exercise05.js
@@ -0,0 +1,10 @@
+// 5 - Dado array de nomes, retornar quantidade de vezes em que aparece a letra 'a' maiúscula ou minúscula.
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  // escreva seu código aqui
+}
