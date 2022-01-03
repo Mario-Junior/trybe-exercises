ccedc37a923f0fc721f42a4a5ec34a3cf3f6c2b1
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-03 17:30:44 -0300

    8.5 learning-exercises update

--- m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/learning-exercises.js
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/learning-exercises.js
@@ -65,3 +65,10 @@ const fruitSalad = (fruit, additional) => [...fruit, ...additional];
 console.log(fruitSalad(specialFruit, additionalItens));
 
 // Parâmetro Rest
+function quantosParams(...args) {
+  console.log('parâmetros:', args);
+  return `Você passou ${args.length} parâmetros para a função.`;
+}
+
+console.log(quantosParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
+console.log(quantosParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
