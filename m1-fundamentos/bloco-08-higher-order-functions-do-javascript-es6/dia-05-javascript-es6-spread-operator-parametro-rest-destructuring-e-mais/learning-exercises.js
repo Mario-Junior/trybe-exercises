a4c08efc342a79e03637767462d717db5237ff44
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-03 15:44:56 -0300

    8.5 learning-exercises update

--- m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/learning-exercises.js
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/learning-exercises.js
@@ -4,3 +4,62 @@ const numbers = [1, 2, 3];
 numbers.push(4);
 
 console.log(numbers); // [1, 2, 3, 4] => o array original foi alterado
+
+const numbers2 = [1, 2, 3];
+const newArray = [...numbers2, 4, 5, 6]; // com spread operator
+console.log(newArray); // [ 1, 2, 3, 4, 5, 6 ]
+console.log(numbers2); // [ 1, 2, 3 ] => o array original não foi alterado
+
+const spring = ['OUT', 'NOV', 'DEZ'];
+const summer = ['JAN', 'FEV', 'MAR'];
+const fall = ['ABR', 'MAI', 'JUN'];
+const winter = ['JUL', 'AGO', 'SET'];
+
+const months = [...summer, ...fall, ...winter, ...spring];
+console.log(months); /* [
+  'JAN', 'FEV', 'MAR',
+  'ABR', 'MAI', 'JUN',
+  'JUL', 'AGO', 'SET',
+  'OUT', 'NOV', 'DEZ'
+] */
+
+// Uso interessante do spread no argumento de função que recebe vários parâmetros.
+const imc = (peso, altura) => (peso / (altura * altura)).toFixed(2);
+const patientInfo = [60, 1.70];
+
+console.log(imc(...patientInfo)); // 20.76
+
+const randomNumbers = [57, 8, 5, 800, 152, 74, 630, 98, 40];
+console.log(Math.max(...randomNumbers)); // 800
+console.log(Math.min(...randomNumbers)); // 5
+
+// com Objetos:
+const people = {
+  id: 101,
+  name: 'Alysson',
+  age: 25,
+};
+
+const about = {
+  address: 'Av. Getúlio Vargas, 1000',
+  occupation: 'Developer',
+};
+
+const customer = { ...people, ...about };
+console.log(customer); /* {
+  id: 101,
+  name: 'Alysson',
+  age: 25,
+  address: 'Av. Getúlio Vargas, 1000',
+  occupation: 'Developer'
+} */
+
+// Para fixar
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['blackberry', 'cherry', 'strawberry'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['canned milk', 'chia', 'orange juice'];
+
+const fruitSalad = (fruit, additional) => [...fruit, ...additional];
+console.log(fruitSalad(specialFruit, additionalItens));
