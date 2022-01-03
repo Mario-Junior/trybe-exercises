2e51533cccb34f69309c78d845ee9e348e62ccdd
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-03 15:18:39 -0300

    8.5 learning-exercises update

---
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/learning-exercises.js
@@ -0,0 +1,6 @@
+// Spread Operator
+// Adicionar itens a um array ... Usando push:
+const numbers = [1, 2, 3];
+numbers.push(4);
+
+console.log(numbers); // [1, 2, 3, 4] => o array original foi alterado
