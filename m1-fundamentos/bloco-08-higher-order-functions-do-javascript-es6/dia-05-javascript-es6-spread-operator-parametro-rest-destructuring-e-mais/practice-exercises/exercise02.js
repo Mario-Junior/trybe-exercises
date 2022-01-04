64d19a60243557659193f4d2c1500946fd7c8751
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-04 08:22:34 -0300

    8.5 practice-exercises 02 update

--- m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/practice-exercises/exercise02.js
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/practice-exercises/exercise02.js
@@ -3,3 +3,10 @@
 // escreva sum abaixo:
 const sum = (...elems) => elems.reduce((acc, curr) => acc + curr, 0);
 console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+const sum2 = (...elems) => elems.reduce(((acc, curr, index) => {
+  console.log('index', index, 'acum', acc);
+  console.log('index', index, 'atual', curr);
+  return acc + curr;
+}), 0);
+console.log(sum2(12, 28, 26));
