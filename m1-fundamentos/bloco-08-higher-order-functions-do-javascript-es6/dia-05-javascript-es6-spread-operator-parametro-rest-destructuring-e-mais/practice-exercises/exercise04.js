92248fb647e7931bf5b5aeb025a2e83e288c26a5
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-04 14:58:34 -0300

    8.5 practice-exercises 04 update

---
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/practice-exercises/exercise04.js
@@ -0,0 +1,31 @@
+// 4 - Escrever função filterPeople que, dada lista de pessoas, retorna todas australianas que nasceram no século 20:
+// Dica: use object destructuring
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo
