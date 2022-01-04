4209fe31ed544b0894dfdfac1fc724d6ecbf6df4
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-04 15:16:31 -0300

    8.5 practice-exercises 05 update

---
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/practice-exercises/exercise05.js
@@ -0,0 +1,6 @@
+// 5 - Escrever função swap , que dado array de 3 elementos, retorna novo array com o primeiro e terceiro elementos trocados.
+// Detalhe: gastando 1 linha só:
+// Dica: use array destructuring
+const myList = [1, 2, 3];
+
+// escreva swap abaixo
