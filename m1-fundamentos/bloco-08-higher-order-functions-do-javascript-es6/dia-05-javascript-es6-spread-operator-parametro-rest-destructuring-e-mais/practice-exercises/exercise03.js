e37b6822a460e9b4d69dfa45c0f2da43b317118d
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-04 08:31:37 -0300

    8.5 practice-exercises 03 update

--- m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/practice-exercises/exercise03.js
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/practice-exercises/exercise03.js
@@ -15,7 +15,7 @@ const gunnar = {
 };
 
 // complete a assinatura da função abaixo
-const personLikes = () => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+const personLikes = ( {name, age, likes} ) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
 
 console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
 console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
