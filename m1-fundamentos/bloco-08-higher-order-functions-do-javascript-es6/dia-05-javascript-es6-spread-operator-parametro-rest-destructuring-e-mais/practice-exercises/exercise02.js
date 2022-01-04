efe9edb4d716ad1985dcf5b4b54ff3f19f34740f
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-04 08:12:08 -0300

    8.5 practice-exercises 02 update

--- m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/practice-exercises/exercise02.js
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/practice-exercises/exercise02.js
@@ -1,3 +1,5 @@
 // 2 - Criar função sum que dado um número variável de elementos retorna a soma desses elementos.
 // Dica: usar parâmetro rest
 // escreva sum abaixo:
+const sum = (...elems) => elems.reduce((acc, curr) => acc + curr, 0);
+console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9));
