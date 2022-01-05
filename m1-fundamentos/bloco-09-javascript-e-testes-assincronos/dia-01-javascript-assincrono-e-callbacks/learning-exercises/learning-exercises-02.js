78509f0680f1d4ed19f1259da05f6d83eb773615
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-05 16:30:51 -0300

    9.1 learning-exercises update

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/learning-exercises/learning-exercises-02.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/learning-exercises/learning-exercises-02.js
@@ -35,3 +35,21 @@ const somaDespesas = (despesas) => {
 };
 
 despesaMensal(renda, despesas, somaDespesas);
+console.log('\n');
+// Para fixar
+// Definição da função userFullName
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+
+// Definição da função getUser
+const getUser = (param) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  // Retornamos nosso parâmetro, que será uma função (callback)
+  return param(userToReturn);
+};
+
+// Chamada/execução da função getUser, que vai receber como parâmetro nossa função userFullName.
+console.log(getUser(userFullName));
