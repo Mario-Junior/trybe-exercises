43dbfd4f6eed83cddc9054b82a566aeb9855613a
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-05 08:11:58 -0300

    8.5 practice-exercises 09 update

---
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/practice-exercises/exercise09.js
@@ -0,0 +1,9 @@
+// 9 - O objeto yearSeasons representam as estações do ano (com base no hemisfério norte). Cada estação é chave do objeto e o valor de cada chave é array de strings, representando os meses da estação. A partir do objeto, desestruture as estações do ano e espalhe-as em um array de meses do ano.
+// Dica : use object destructuring e spread operator
+
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
