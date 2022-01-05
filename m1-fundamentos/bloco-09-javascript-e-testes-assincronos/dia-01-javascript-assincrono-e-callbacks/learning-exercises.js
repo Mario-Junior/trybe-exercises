79382d96d933de261877270f52e6747cca26877e
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-05 16:00:12 -0300

    9.1 learning-exercises update

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/learning-exercises.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/learning-exercises.js
@@ -1,2 +1,23 @@
 //* Bloco 09 - Dia 01 
 //* Operações assíncronas *//
+console.log('1 - Receber roda');
+console.log('2 - Encaixar parafusos');
+console.log('3 - Fixar roda no carro');
+console.log('\n');
+
+console.log('1 - Comprar parafusos');
+console.log('2 - Adicionar ao estoque');
+console.log('3 - Receber roda');
+console.log('4 - Encaixar parafusos');
+console.log('5 - Fixar roda no carro');
+console.log('\n');
+
+setTimeout(() => {
+  console.log('Comprar parafusos') // Comprar parafusos
+  console.log('Adicionar ao estoque') // Adicionar ao estoque
+}, 2000);
+console.log('1 - Receber roda'); // 1 - Receber roda
+console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
+console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro
+console.log('\n');
+
