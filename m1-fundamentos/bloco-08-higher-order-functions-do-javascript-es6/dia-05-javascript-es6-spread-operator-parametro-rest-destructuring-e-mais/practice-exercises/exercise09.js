f4562c46742435e5b891cb1615eaff7dce8257db
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-05 08:14:22 -0300

    8.5 practice-exercises 09 update

--- m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/practice-exercises/exercise09.js
+++ m1-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/practice-exercises/exercise09.js
@@ -7,3 +7,12 @@ const yearSeasons = {
   autumn: ['September', 'October', 'November'],
   winter: ['December', 'January', 'February'],
 };
+
+const yearMonths = ( {spring, summer, autumn, winter} ) => [...spring, ...summer, ...autumn, ...winter];
+console.log(yearMonths(yearSeasons));
+
+// Gabarito:
+const { spring, summer, autumn, winter } = yearSeasons;
+const months = [...spring, ...summer, ...autumn, ...winter];
+
+console.log(months); // ['March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'January', 'February']
