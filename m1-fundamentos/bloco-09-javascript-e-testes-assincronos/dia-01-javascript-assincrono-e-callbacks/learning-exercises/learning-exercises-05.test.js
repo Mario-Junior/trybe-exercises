bd7511dbb546525eac67ea6e696175ddb94161b7
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-11 13:04:00 -0300

    9.1 learning-exercises upload

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/learning-exercises/learning-exercises-05.test.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/learning-exercises/learning-exercises-05.test.js
@@ -11,3 +11,23 @@ const addCity = (city) => {
 const removeCity = (city) => {
   cities = cities.filter((eachCity) => eachCity !== city);
 };
+
+test('Testa a função addCity', () => {
+  expect.assertions(4);
+  addCity('Campinas');
+  addCity('Goiania');
+  addCity('Recife');
+  expect(cities).toHaveLength(3);
+  expect(cities).toContain('Campinas');
+  expect(cities).toContain('Goiania');
+  expect(cities).toContain('Recife');
+});
+
+test('Testa a função removeCity', () => {
+  expect.assertions(4);
+  removeCity('Campinas');
+  expect(cities).toHaveLength(2);
+  expect(cities).not.toContain('Campinas');
+  expect(cities).toContain('Goiania');
+  expect(cities).toContain('Recife');
+});
