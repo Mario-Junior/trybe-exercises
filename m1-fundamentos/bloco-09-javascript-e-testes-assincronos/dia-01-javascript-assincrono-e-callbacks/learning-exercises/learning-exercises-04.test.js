73631839cd14a3d8ff41fa97e43ebd33a654e055
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-11 11:39:59 -0300

    9.1 learning-exercises upload

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/learning-exercises/learning-exercises-04.test.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/learning-exercises/learning-exercises-04.test.js
@@ -44,3 +44,22 @@ test('Não deveria passar!', (done) => {
     }
   }, 500);
 });
+
+// Exemplo de fixação com uma função que espera um tempo e retorna a soma de dois números
+const asyncSum = (a, b, callback) => {
+  setTimeout(() => {
+    const result = a + b;
+    callback(result);
+  }, 500);
+};
+
+test('Testando asyncSum, soma 5 mais 10', (done) => {
+  asyncSum(3, 12, (result) => {
+    try {
+      expect(result).toBe(15);
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
