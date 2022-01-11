b9f2af6b45f1feac60569601a58a2c8b88dd0ed6
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-11 15:15:21 -0300

    9.1 practice-exercises-01 upload

---
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/practice-exercise-01.js
@@ -0,0 +1,33 @@
+// 1 - Dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+console.log(planetDistanceFromSun(venus)); // B
+console.log(planetDistanceFromSun(jupiter)); // C
+
+// Minha resposta: 1 - A, 2 - B, 3 - C.
