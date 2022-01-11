3760047a94a90ee1be71c050a39df339c886d871
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-11 11:31:53 -0300

    9.1 learning-exercises upload

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/learning-exercises/learning-exercises-04.test.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/learning-exercises/learning-exercises-04.test.js
@@ -1,6 +1,6 @@
 //* Bloco 09 - Dia 01 *//
 //* Testes Assíncronos com Callbacks *//
-Este teste, gera falso positivo, pois o teste só roda o bloco 'false' do código, parte assíncrona, após o timeout;
+// Este teste, gera falso positivo, pois o teste só roda o bloco 'false' do código, parte assíncrona, após o timeout;
 test('Não deveria passar!', () => {
   setTimeout(() => {
     expect(10).toBe(5);
@@ -18,7 +18,8 @@ test('Não deveria passar!', (done) => {
   }, 500);
 });
 
-
+// Refatorando e tratando o erro com o bloco try/catch:
+// Mas o teste ainda retorna falso-positivo pois, como encerramos com 'done' após o erro, o teste é interpretado como correto.
 test('Não deveria passar!', (done) => {
   setTimeout(() => {
     try {
@@ -27,6 +28,19 @@ test('Não deveria passar!', (done) => {
       done();
     } catch (error) {
       done();
+    }
+  }, 500);
+});
+
+// Refatorando para finalmente a callback pegar (catch) o erro lançado pela callback 'done'
+test('Não deveria passar!', (done) => {
+  setTimeout(() => {
+    try {
+      expect(10).toBe(5);
+      console.log('Deveria falhar!');
+      done();
+    } catch (error) {
+      done(error); // Alteramos esta linha
     }
   }, 500);
 });
