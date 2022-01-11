ac0418f90873bd76170bde63b806a9c89d15a57e
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-11 11:20:43 -0300

    9.1 learning-exercises upload

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/learning-exercises/learning-exercises-04.test.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/learning-exercises/learning-exercises-04.test.js
@@ -1,8 +1,32 @@
 //* Bloco 09 - Dia 01 *//
 //* Testes Assíncronos com Callbacks *//
+Este teste, gera falso positivo, pois o teste só roda o bloco 'false' do código, parte assíncrona, após o timeout;
 test('Não deveria passar!', () => {
   setTimeout(() => {
     expect(10).toBe(5);
     console.log('Deveria falhar!');
+  }, 500);
+});
+
+// Reescrevendo o teste com a callback 'done' que é a biblioteca do Jest para chamar após os testes assíncronos.
+// Porém, ele falha com uma mensagem de timeout.
+test('Não deveria passar!', (done) => {
+  setTimeout(() => {
+    expect(10).toBe(5);
+    console.log('Deveria falhar!');
+    done();
+  }, 500);
+});
+
+
+test('Não deveria passar!', (done) => {
+  setTimeout(() => {
+    try {
+      expect(10).toBe(5);
+      console.log('Deveria falhar!');
+      done();
+    } catch (error) {
+      done();
+    }
   }, 500);
 });
