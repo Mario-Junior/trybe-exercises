56653681cba8ed9c11278dd698f2843f5bf5a440
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-12 20:59:11 -0300

    9.2 learning-exercises upload

---
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/fetch.js
@@ -0,0 +1,11 @@
+//* Fetch API *//
+// Gestores de fluxo
+const fetch = require('node-fetch');
+
+const fetchJoke = () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  console.log(fetch(url));
+}
+
+fetchJoke();
