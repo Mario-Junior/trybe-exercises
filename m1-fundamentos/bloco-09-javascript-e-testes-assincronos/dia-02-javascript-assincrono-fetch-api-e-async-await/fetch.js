927a54150b77cf94bfa20ec4c7932b0473c90cc8
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-12 21:31:40 -0300

    9.2 learning-exercises upload

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/fetch.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/fetch.js
@@ -21,3 +21,17 @@ const fetchJoke = () => {
     .then((data) => console.log(data.value));
 }
 fetchJoke();
+
+// Adicionando .catch() ao código para tratar possíveis erros 
+// * Para forçar um erro, foi retirado o https:// do início da url
+const fetch = require('node-fetch');
+
+const fetchJoke = () => {
+  const url = 'api.chucknorris.io/jokes/random?category=dev';
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+}
+fetchJoke(); // Algo deu errado :( // TypeError: Only absolute URLs are supported
