d3b06e91ba01d9fbf2cd4291dff47568322e21cf
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-13 08:16:48 -0300

    9.2 practice-exercise-01 upload

---
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/practice-exercise-01/apiScript.js
@@ -0,0 +1,9 @@
+// Exercício Prático 01
+// apiScript.js
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  // Adicionar lógica aqui!
+};
+
+window.onload = () => fetchJoke();
