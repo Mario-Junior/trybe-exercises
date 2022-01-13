cddd540b9ac66ec71744a962840d73d8414deeac
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-13 19:03:36 -0300

    9.3 learning-exercises upload

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/9.3-content-async-test/tests/fetchCharacter.test.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/9.3-content-async-test/tests/fetchCharacter.test.js
@@ -6,4 +6,8 @@ describe('Testes para a função fetchCharacter', () => {
     const character = await fetchCharacter('720');
     expect(character.name).toBe('Wonder Woman');
   });
+  it('Verifica se retorna erro ao executar a função sem parâmetro', async () => {
+    const failRequest = await fetchCharacter();
+    console.log(failRequest);
+  });
 });
