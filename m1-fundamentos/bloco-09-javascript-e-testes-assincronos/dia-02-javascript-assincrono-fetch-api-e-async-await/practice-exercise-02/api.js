815cf9cf86f9ab7d9a853a874b188b2f08820c70
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-13 14:41:00 -0300

    9.2 practice-exercise-02 upload

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/practice-exercise-02/api.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/practice-exercise-02/api.js
@@ -1,5 +1,5 @@
 // Exercício Prático 02
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
 const fetchCriptos = async () => {
   const apiUrl = 'https://api.coincap.io/v2/assets';
@@ -7,8 +7,21 @@ const fetchCriptos = async () => {
   const coinsArr = await fetch(apiUrl)
     .then(response => response.json())
     .then((data) => data.data)
-    .catch((error) => console.log(error));
-
-    console.log(coinsArr);
+    .catch((error) => error.toString());
+  return coinsArr;
 };
-fetchCriptos();
+
+const showCoins = async () => {
+  const coins = await fetchCriptos();
+  const coinsList = document.getElementById('cripto-list');
+
+  coins.forEach(coin => {
+    const newLi = document.createElement('li');
+    const usdPrice = Number(coin.priceUsd);
+
+    newLi.innerText = `${coin.name} (${coin.symbol}): USD ${usdPrice.toFixed(2)}`;
+    coinsList.appendChild(newLi);
+  });
+}
+
+window.onload = () => showCoins();
