26a3e4b596d4547bb65b05800fc5880d9cb9e1d6
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-12 21:43:06 -0300

    9.2 learning-exercises upload

---
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/asyncAwait.js
@@ -0,0 +1,13 @@
+//* Async e Await *//
+// Refatorando a função anterior com async
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+}
+fetchJoke();
