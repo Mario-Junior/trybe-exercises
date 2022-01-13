d0316cc2a93020018926dffcdd08134f34dcb5c2
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-13 19:28:22 -0300

    9.3 learning-exercises upload

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/9.3-content-async-test/tests/fetchCharacter.test.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/9.3-content-async-test/tests/fetchCharacter.test.js
@@ -14,4 +14,10 @@ describe('Testes para a função fetchCharacter', () => {
     const response = await fetchCharacter('parametro qualquer');
     expect(response).toBe('Invalid id');
   });
+  it('Verifica se fetch é chamada com o endpoint correto', async () => {
+    const url = 'https://www.superheroapi.com/api.php/4192484924171229/720';
+    await fetchCharacter('720');
+    expect(fetch).toHaveBeenCalledTimes(4);
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
 });
