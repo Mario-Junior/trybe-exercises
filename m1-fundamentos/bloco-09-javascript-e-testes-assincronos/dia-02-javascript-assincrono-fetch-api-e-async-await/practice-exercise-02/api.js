1010476518910d4eff29a013473f37074da00922
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-13 13:57:42 -0300

    9.2 practice-exercise-02 upload

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/practice-exercise-02/api.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/practice-exercise-02/api.js
@@ -1,15 +1,14 @@
 // Exercício Prático 02
 const fetch = require('node-fetch');
 
-const apiUrl = 'https://api.coincap.io/v2/assets';
-
-const fetchCriptos = () => {
-  fetch(apiUrl)
+const fetchCriptos = async () => {
+  const apiUrl = 'https://api.coincap.io/v2/assets';
+  
+  const coinsArr = await fetch(apiUrl)
     .then(response => response.json())
-    .then((data) => {
-      console.log(data);
-      return data;
-    })
+    .then((data) => data.data)
     .catch((error) => console.log(error));
+
+    console.log(coinsArr);
 };
 fetchCriptos();
