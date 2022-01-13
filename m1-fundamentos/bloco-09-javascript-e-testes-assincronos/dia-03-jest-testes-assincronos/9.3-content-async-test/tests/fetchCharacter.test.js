fe70b39edf1fe3dcf93d2762ae605898641386ac
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-13 19:01:37 -0300

    9.3 learning-exercises upload

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/9.3-content-async-test/tests/fetchCharacter.test.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/9.3-content-async-test/tests/fetchCharacter.test.js
@@ -2,7 +2,8 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Testes para a função fetchCharacter', () => {
-  it('', async () => {
-    await require();
+  it('Verifica se o nome da personagem é Wonder Woman', async () => {
+    const character = await fetchCharacter('720');
+    expect(character.name).toBe('Wonder Woman');
   });
 });
