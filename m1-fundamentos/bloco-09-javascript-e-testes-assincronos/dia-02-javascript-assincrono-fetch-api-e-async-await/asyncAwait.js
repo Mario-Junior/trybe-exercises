7053451e57704a99d37623896d757995bb9bae80
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-12 21:48:32 -0300

    9.2 learning-exercises upload

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/asyncAwait.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-02-javascript-assincrono-fetch-api-e-async-await/asyncAwait.js
@@ -11,3 +11,35 @@ const fetchJoke = async () => {
     .catch((error) => console.log(`Algo deu errado :( \n${error}`));
 }
 fetchJoke();
+
+// Completando com await que faz a função esperar uma resposta para continuação de sua execução
+// Usando mesclado com .then() e .catch()
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  const result = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.value)
+    .catch((error) => `Algo deu errado :( \n${error}`);
+  
+  console.log(result);
+};
+fetchJoke();
+
+// Usando sem .then() com try/catch
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.value);
+  } catch(error) {
+    console.log(`Algo deu errado :( \n${error}`);
+  }
+}
+fetchJoke();
