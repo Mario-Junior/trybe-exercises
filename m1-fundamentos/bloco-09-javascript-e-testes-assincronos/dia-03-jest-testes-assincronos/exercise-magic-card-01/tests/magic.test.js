623cd074c9067cf6e005ffe61c4894ed9abe8fab
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-13 21:09:26 -0300

    9.3 practice-exercise-01 upload

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/exercise-magic-card-01/tests/magic.test.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/exercise-magic-card-01/tests/magic.test.js
@@ -3,9 +3,13 @@ const { getMagicCard } = require('../src/magic.js');
 const { card } = require('../simulator/card');
 
 describe('Testa a função getMagicCard', () => {
-  it('Deve possuir a propriedade name com o valor Ancestor\'s Chosen', async () => {
+  // 1 - Completei a sintaxe com async antes do it e await antes da chamada da função getMagicCard
+  it('2- Deve possuir a propriedade name com o valor Ancestor\'s Chosen', async () => {
     const response = await getMagicCard('130550');
     const { name } = response;
     expect(name).toBe(`Ancestor\'s Chosen`);
+  });
+  it('3 - getMagicCard é uma função', async () => {
+    expect(typeof getMagicCard).toBe('function');
   });
 });
