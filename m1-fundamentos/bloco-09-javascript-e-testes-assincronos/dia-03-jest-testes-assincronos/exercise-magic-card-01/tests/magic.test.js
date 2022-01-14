60e3c4f669b8e29cbcb2bf9ec8da998c703931cf
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-13 21:15:12 -0300

    9.3 practice-exercise-01 upload

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/exercise-magic-card-01/tests/magic.test.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/exercise-magic-card-01/tests/magic.test.js
@@ -4,15 +4,19 @@ const { card } = require('../simulator/card');
 
 describe('Testa a função getMagicCard', () => {
   // 1 - Completei a sintaxe com async antes do it e await antes da chamada da função getMagicCard
-  it('2- Deve possuir a propriedade name com o valor Ancestor\'s Chosen', async () => {
+  it('2- Deve possuir a propriedade name com o valor "Ancestor\'s Chosen".', async () => {
     const response = await getMagicCard('130550');
     const { name } = response;
     expect(name).toBe(`Ancestor\'s Chosen`);
   });
-  it('3- getMagicCard é uma função', async () => {
+  it('3- getMagicCard é uma função.', async () => {
     expect(typeof getMagicCard).toBe('function');
   });
   it('4- Com o argumento "130550", a função fetch é chamada.', async () => {
+    await getMagicCard('130550');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('5- Com o argumento "130550", a função fetch foi chamada com o endpoint "https://api.magicthegathering.io/v1/cards/130550".', async () => {
     await getMagicCard('130550');
     expect(fetch).toHaveBeenCalled();
   });
