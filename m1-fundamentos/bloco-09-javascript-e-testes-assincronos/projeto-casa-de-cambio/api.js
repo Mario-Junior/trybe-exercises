bcf06855eda2c07599951941e4903b8ed1aa2939
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-17 17:03:00 -0300

    Exchange Project api.js refactoring to async/await

--- m1-fundamentos/bloco-09-javascript-e-testes-assincronos/projeto-casa-de-cambio/api.js
+++ m1-fundamentos/bloco-09-javascript-e-testes-assincronos/projeto-casa-de-cambio/api.js
@@ -1,12 +1,17 @@
 // const fetch = require('node-fetch');
 
-const fetchCurrency = (currency) => {
-  return fetch(`https://api.exchangerate.host/latest?base=${currency}`)
-  .then((response) => response.json())
-  .then((object) => {
-    handleRates(object.rates);
-    handleBaseCurrency(object.base);
-  })
+const fetchCurrency = async (currency) => {
+  const response = await fetch(`https://api.exchangerate.host/latest?base=${currency}`);
+  const object = await response.json();
+  handleRates(object.rates);
+  handleBaseCurrency(object.base);
+
+  // return fetch(`https://api.exchangerate.host/latest?base=${currency}`)
+  // .then((response) => response.json())
+  // .then((object) => {
+  //   handleRates(object.rates);
+  //   handleBaseCurrency(object.base);
+  // })
 }
 fetchCurrency();
 
