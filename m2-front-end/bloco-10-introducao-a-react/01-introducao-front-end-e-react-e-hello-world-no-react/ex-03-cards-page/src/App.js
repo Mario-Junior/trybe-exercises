2edf8f9a54ef0d04a7f5785bbb525d7e103a2046
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-21 15:35:10 -0300

    10.1 practice-exercises 03 update

--- m2-front-end/bloco-10-introducao-a-react/01-introducao-front-end-e-react-e-hello-world-no-react/ex-03-cards-page/src/App.js
+++ m2-front-end/bloco-10-introducao-a-react/01-introducao-front-end-e-react-e-hello-world-no-react/ex-03-cards-page/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Content from './Content';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
       <Header />
       <Content />
+      <Footer />
     </div>
   );
 }
---
+++ m2-front-end/bloco-10-introducao-a-react/01-introducao-front-end-e-react-e-hello-world-no-react/ex-03-cards-page/src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react/cjs/react.production.min';
+
+class Footer extends Component {
+  render() {
+    return (
+      <h2>E isso é só o começo...</h2>
+    );
+  }
+}
+
+export default Footer;
