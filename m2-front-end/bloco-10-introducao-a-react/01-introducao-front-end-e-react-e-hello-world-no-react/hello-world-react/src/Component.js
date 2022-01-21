f598924601cf5fbe1336218f2ee631b9b1aa49b3
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-21 13:41:48 -0300

    10.1 learning-exercises - finally hello world

---
+++ m2-front-end/bloco-10-introducao-a-react/01-introducao-front-end-e-react-e-hello-world-no-react/hello-world-react/src/Component.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class MyName extends Component {
+  render() {
+    return (
+    <div className='Text'>
+      <h1>Mário Júnior</h1>
+      <p>An eternal learner!</p>
+      <h2>Hello, World!!!</h2>
+      <p>Let's learn React!</p>
+    </div>
+    )
+  }
+}
+
+export default MyName;
