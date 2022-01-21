6d0e049c037b9f994aabc29970fe25438e7a23e2
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-21 15:18:01 -0300

    10.1 practice-exercises 03 update

---
+++ m2-front-end/bloco-10-introducao-a-react/01-introducao-front-end-e-react-e-hello-world-no-react/ex-03-cards-page/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react/cjs/react.production.min';
+
+class Header extends Component {
+  render() {
+    return (
+      <h1>Conteúdos de Front-End</h1>
+    );
+  }
+}
+
+export default Header;
