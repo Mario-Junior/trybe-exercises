499d6217c18e045a59119080f3453e5b3765b1d3
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-23 19:35:07 -0300

    10.2 learning-exercises update

---
+++ m2-front-end/bloco-10-introducao-a-react/02-componentes-react/learning-exercise-04/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
+    const items = shoppingList.map((item) => {
+      console.log("item: ", item);
+      return (<li>{ item }</li>);
+    });
+
+    return (
+      <div>
+        <h2>Lista de compras</h2>
+        <ul>
+          { items }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
