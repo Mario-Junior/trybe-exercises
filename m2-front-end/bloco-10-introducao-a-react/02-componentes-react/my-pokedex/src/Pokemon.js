3ed93a1ef232d233a89cb320033427a86a572e96
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-24 08:40:57 -0300

    10.2 practice-exercise update

---
+++ m2-front-end/bloco-10-introducao-a-react/02-componentes-react/my-pokedex/src/Pokemon.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import PropTypes from 'prop-types';
+
+class Pokemon extends Component {
+  render () {
+    const { pokemon: { name, type, averageWeight, image } } = this.props;
+    return (
+      <div className="pokemon">
+          <div key={ pokemon.props.id } className="card">
+            <h2>{ name }</h2>
+            <p>{ type }</p>
+            <p>{ `Average weight: ${ averageWeight.value } ${ averageWeight.measurementUnit }` }</p>
+          </div>
+          <img src={ image } alt={ `Pokemon ${ name } sprite` }/>
+      </div>
+    );
+  }
+}
+
+Pokemon.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  type: PropTypes.string,
+};
+
+export default Pokemon;
