00f95b3d2c71a708ed3297b43bce8e40391bfbe5
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-26 20:52:11 -0300

    11.1 learning-exercise-01 update

--- m2-front-end/bloco-11-componentes-com-estado-eventos-formularios-com-react/01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ m2-front-end/bloco-11-componentes-com-estado-eventos-formularios-com-react/01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Para fixar
+// 1 - Crie uma aplicação React com npx create-react-app fancy-buttons . Altere o componente App.js para que seja um componente de classe e contenha um botão associado a um evento que imprime um texto qualquer via console.log() . Não precisa se preocupar com a sintaxe correta para funções de eventos por enquanto!!
+// 2 - Faça com que sua aplicação exiba três botões lado a lado com textos diferentes. Cada botão clicado deve acionar um evento diferente, cada um escrevendo algo diferente no console do navegador via console.log() .
+// 3 - Declare dentro da classe do seu componente dos exercícios de fixação acima a função que lida com o evento que antes era lidado por uma função do lado de fora da classe!
+// 4 - Garanta acesso ao objeto this na função que você declarou.
+// 5 - Agora você vai converter sua aplicação para uma que conta o número de cliques dado em cada botão! Primeiramente, defina um estado inicial para a contagem de cada botão.
+// Dica: Uma possibilidade é você definir três chaves, uma para cada botão!
+// 6 - Agora, quando um botão for clicado, altere de forma assíncrona o estado deste botão de zero para um.
+// 7 - Por fim, baseie-se no estado anterior ao atual para incrementar a contagem de cliques cada vez que um botão for clicado!
+
+
 // Versão 1 (Descomente da linha 5 até a linha 28 para ver)
 // /* Embora isso funcione, essa DEFINITIVAMENTE
 // não é a maneira correta de se criar eventos
@@ -80,7 +91,7 @@ import './App.css';
 // }
 
 // Refatoração 3 = Incluindo estado (state e setState)
-// Descomente da linha  até a linha  para ver)
+// Descomente da linha 95 até a linha 138 para ver)
 class App extends Component {
   constructor() {
     super()
@@ -89,7 +100,9 @@ class App extends Component {
     this.handleOnMouseOver = this.handleOnMouseOver.bind(this);
     this.handleOnLoadFocus = this.handleOnLoadFocus.bind(this);
     this.state = {
-      clicksNumber: 0
+      clicksNumber: 0,
+      overNumber: 0,
+      focusNumber: 0,
     }
   }
   
@@ -97,18 +110,28 @@ class App extends Component {
     this.setState((previousState, _props) => ({
       clicksNumber: previousState.clicksNumber + 1
     }))
-    console.log('Clicou no botão! E chamou o `this`', this)
+    console.log('Clicou no botão! E chamou o `this`', this);
   };
-  handleOnMouseOver = () => console.log('Passou o mouse aqui, hein! ;D');
-  handleOnLoadFocus = () => console.log('Oh o foco aqui em mim! XD');
-  
+  handleOnMouseOver() {
+    this.setState((previousState, _props) => ({
+      overNumber: previousState.overNumber + 1
+    }))
+    console.log('Passou o mouse aqui, hein! ;D');
+  };
+  handleOnLoadFocus() {
+    this.setState((previousState, _props) => ({
+      focusNumber: previousState.focusNumber + 1
+    }))
+    console.log('Oh o foco aqui em mim! XD');
+  };
+    
   render () {
     return (
       <>
         <h3>Abra o Console (F12) para ver os resultados de cada botão</h3>
         <button onClick={this.handleClick}>Clique aqui: {this.state.clicksNumber}</button>
-        <button onMouseOver={this.handleOnMouseOver}>Passe o mouse</button>
-        <button onFocus={this.handleOnLoadFocus} autoFocus>Coloque o foco</button>
+        <button onMouseOver={this.handleOnMouseOver}>Passe o mouse: {this.state.overNumber}</button>
+        <button onFocus={this.handleOnLoadFocus} autoFocus>Coloque o foco: {this.state.focusNumber}</button>
       </>
     ) 
   }
