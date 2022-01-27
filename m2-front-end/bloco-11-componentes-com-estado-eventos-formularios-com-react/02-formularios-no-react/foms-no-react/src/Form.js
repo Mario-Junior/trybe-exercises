bec79aa86d7ccf8845be519931f49285298ad0c6
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-27 16:20:55 -0300

    11.2 learning-exercises update

--- m2-front-end/bloco-11-componentes-com-estado-eventos-formularios-com-react/02-formularios-no-react/foms-no-react/src/Form.js
+++ m2-front-end/bloco-11-componentes-com-estado-eventos-formularios-com-react/02-formularios-no-react/foms-no-react/src/Form.js
@@ -1,26 +1,70 @@
 import React, { Component } from 'react';
+import './Form.css';
 
 class Form extends Component {
   constructor() {
     super();
 
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      aprendizados: '',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
   }
 
   render () {
     return (
       <>
         <h1>Estudando Formulários em React - Exercício de aprendizado</h1>
-        <form className='form'>
+        <form className="form">
           <label>
             Escolha seu aprendizado favorito até aqui:
-            <select name='aprendizadoFavorito'>
-              <option value='html'>HTML</option>
-              <option value='css'>CSS</option>
-              <option value='javascript'>JavaScript</option>
-              <option value='react'>React</option>
+            <select
+              name="aprendizadoFavorito"
+              value={this.state.aprendizadoFavorito}
+              onChange={this.handleChange}
+            >
+              <option value="html">HTML</option>
+              <option value="css">CSS</option>
+              <option value="javascript">JavaScript</option>
+              <option value="react">React</option>
             </select>
           </label>
-          
+          <label>
+            Seu nome aqui:
+            <input
+              type="name"
+              name="nome"
+              value={this.state.nome}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Seu e-mail aqui:
+            <input
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Diga mais sobre seus aprendizados até aqui!
+            <textarea
+              name="aprendizados"
+              value={this.state.aprendizados}
+              onChange={this.handleChange}
+            />
+          </label>
         </form>
       </>
     )
