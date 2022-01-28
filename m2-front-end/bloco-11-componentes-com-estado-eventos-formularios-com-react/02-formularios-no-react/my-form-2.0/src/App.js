d915baedf89a3728c4a5246c4966f8aa148a6b5f
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-28 10:59:30 -0300

    11.2 practice-exercises update

--- m2-front-end/bloco-11-componentes-com-estado-eventos-formularios-com-react/02-formularios-no-react/my-form-2.0/src/App.js
+++ m2-front-end/bloco-11-componentes-com-estado-eventos-formularios-com-react/02-formularios-no-react/my-form-2.0/src/App.js
@@ -23,6 +23,52 @@ class App extends Component {
     this.state = initial_state;
   }
 
+  changeHandler = event => {
+    let { name, value } = event.target;
+
+    if (name === 'name') value = value.toUpperCase();
+    if (name === 'address') value = this.validateAddress(value);
+
+    this.updateState(name, value);
+  }
+
+  onBlurHandler = event => {
+    let { name, value } = event.target;
+
+    if (name === 'city') value = value.match(/^\d/) ? '' : value;
+
+    this.updateState(name, value);
+  }
+
+  updateState(name, value) {
+    this.setState((state) => ({
+      [name]: value,
+      formError: {
+        ... state.formError,
+        [name]: this.validateField(name, value)
+      }
+    }));
+  }
+
+  validateAddress = address => address.replace(/[^\w\s]/gi, '');
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  validateField(fieldName, value) {
+    switch (fieldName) {
+      case 'email':
+        const isValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2})$/i)
+        return isValid ? '' : ' is invalid';
+      default:
+        break;
+    }
+    return '';
+  }
+
+  
+
   render() {
     return (
       <>
