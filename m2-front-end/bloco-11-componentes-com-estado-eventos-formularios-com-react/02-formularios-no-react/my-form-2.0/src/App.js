1909dc3e64881b262b6dfeb04101ad4f1035256a
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-01-28 10:37:03 -0300

    11.2 practice-exercises update

--- m2-front-end/bloco-11-componentes-com-estado-eventos-formularios-com-react/02-formularios-no-react/my-form-2.0/src/App.js
+++ m2-front-end/bloco-11-componentes-com-estado-eventos-formularios-com-react/02-formularios-no-react/my-form-2.0/src/App.js
@@ -2,7 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 import Form from './Form';
 
+const initial_state = {
+  name: '',
+  email: '',
+  cpf: '',
+  address: '',
+  city: '',
+  countryState: '',
+  addresType: '',
+  resume: '',
+  role: '',
+  roleDescription: '',
+  formError: {},
+  sudmitted: false,
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = initial_state;
+  }
+
   render() {
     return (
       <>
