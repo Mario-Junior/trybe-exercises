a5a1b09bdf72a044286f7bb932317bc9848578ed
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-02-01 15:01:18 -0300

    12.1 practice-exercises update

--- m2-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/01-ciclo-de-vida-de-componentes/joke-app/src/App.js
+++ m2-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/01-ciclo-de-vida-de-componentes/joke-app/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class DadJoke extends Component {
+  constructor() {
+    super();
+
+    this.saveJoke = this.saveJoke.bind(this);
+    this.renderJokeElement = this.renderJokeElement.bind(this);
+
+    this.state = {
+      jokeObj: undefined,
+      loading: true,
+      storedJokes: [],
+    }
+  }
+
+  async fetchJoke() {
+    const requestHeaders = { headers: { Accept: 'application/json' } }
+    const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
+    const requestObject = await requestReturn.json();
+    this.setState({
+      jokeObj: requestObject,
+    })
+  }
+
+  componentDidMount() {
+    this.fetchJoke();
+  }
+
+  saveJoke() {
+    //Salvando a piada no array de piadas existentes
+
+  }
+
+  renderJokeElement() {
+    return (
+      <div>
+        <p>{this.state.jokeObj.joke}</p>
+        <button type="button" onClick={this.saveJoke}>
+          Salvar piada!
+        </button>
+      </div>
+    );
+  }
+
+  render() {
+    const { storedJokes } = this.state;
+    const loadingElement = <span>Loading...</span>;
+
+    return (
+      <div>
+        <span>
+          {storedJokes.map(({ id, joke }) => (<p key={id}>{joke}</p>))}
+        </span>
+
+      <span>RENDERIZAÇÃO CONDICIONAL</span>
+
+      </div>
+    );
+  }
 }
 
-export default App;
+export default DadJoke;
