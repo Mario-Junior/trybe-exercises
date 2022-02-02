8a854388de422d7e233709d95b00fe2e4d70701b
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-02-02 15:58:14 -0300

    12.2 exercises & folders updates

---
+++ m2-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/02-react-router/comprehension-exercises-router/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <h1>Meu App React - Treinando React Router Dom</h1>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+      </BrowserRouter>
+    );
+  }
+}
+
+
+export default App;
