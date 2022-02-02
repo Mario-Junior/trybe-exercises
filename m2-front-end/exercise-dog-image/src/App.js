2e04cdf8770a4e06ebb59eae29ea339bd8771a9b
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-02-02 00:06:19 -0300

    12.1 practice-exercises update

--- m2-front-end/exercise-dog-image/src/App.js
+++ m2-front-end/exercise-dog-image/src/App.js
@@ -2,10 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      dog: {},
+      loading: true,
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const require = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await require.json();
+
+      console.log(data.message.split('/'));
+      this.setState({ 
+        dog: data,
+        loading: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  shouldComponentUpdate(_nextProps, nextState) {
+    const breed = 'terrier';
+
+    if (nextState.dog.message.includes(breed)) {
+      return false;
+    }
+    return true;
+  }
+
+  componentDidUpdate() {
+    
+  }
+
+  refreshPage = () => window.location.reload();
+
   render() {
+    const { dog, loading } = this.state;
+    const loadingElement = <p><span role="img" aria-label="dog">🐶 Loading...</span></p>
+    const dogImg = <div><img src={dog.message} alt="doguinho"></img></div>
     return(
       <div>
-        🐶
+        <button type="button" onClick={this.refreshPage}>Novo Doguinho</button>
+        { loading ? loadingElement : dogImg }
       </div>
     )
   }
