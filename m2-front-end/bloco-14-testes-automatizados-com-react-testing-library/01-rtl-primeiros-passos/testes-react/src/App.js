571b6a417d6303fada1f86c62d874d6fd9447227
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-02-15 19:22:12 -0300

    14.1 learning exercises update

--- m2-front-end/bloco-14-testes-automatizados-com-react-testing-library/01-rtl-primeiros-passos/testes-react/src/App.js
+++ m2-front-end/bloco-14-testes-automatizados-com-react-testing-library/01-rtl-primeiros-passos/testes-react/src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
           onClick={ () => this.changeSaveEmail(email) }
         />
         <input id="btn-id" type="button" value="Voltar" />
-        <h2 data-testid="id-email-user">{`Valor: ${saveEmail}`}</h2>
+        <ValidEmail email={ saveEmail } />
       </div>
     );
   }
