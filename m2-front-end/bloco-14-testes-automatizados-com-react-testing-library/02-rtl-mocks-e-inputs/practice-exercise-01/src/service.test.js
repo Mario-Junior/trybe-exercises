15d1b066038a9929e10d2e79a447f30d39d4fbda
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-02-16 19:13:20 -0300

    14.2 practice-exercise update

--- m2-front-end/bloco-14-testes-automatizados-com-react-testing-library/02-rtl-mocks-e-inputs/practice-exercise-01/src/service.test.js
+++ m2-front-end/bloco-14-testes-automatizados-com-react-testing-library/02-rtl-mocks-e-inputs/practice-exercise-01/src/service.test.js
@@ -1,5 +1,16 @@
 const service = require('./service');
 
-test('Se a função returnRandomNumber é chamada', () => {
-  expect(returnRandomNumber).toBeCalled;
-});
+describe('Exercício Prático - Parte 1', () => {
+  test('Se a função returnRandomNumber é chamada', () => {
+    service.returnRandomNumber = jest.fn().mockReturnValue(10);
+    expect(service.returnRandomNumber).toHaveBeenCalled;
+  });
+  test('Se a função returnRandomNumber é chamada apenas 1 vez', () => {
+    service.returnRandomNumber = jest.fn().mockReturnValue(10);
+    expect(service.returnRandomNumber).toHaveBeenCalledTimes(1);
+  });
+  test('Se o retorno da função é o number 10', () => {
+    service.returnRandomNumber = jest.fn().mockReturnValue(10);
+    expect(service.returnRandomNumber()).toBe(10);
+  });
+})
