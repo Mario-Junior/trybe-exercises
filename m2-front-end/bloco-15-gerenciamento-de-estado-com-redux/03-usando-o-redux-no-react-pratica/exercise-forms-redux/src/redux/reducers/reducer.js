7f17a998760f41fff776dc5839eba3056b6cccab
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-02-24 16:02:18 -0300

    exercise-form-redux update

---
+++ m2-front-end/bloco-15-gerenciamento-de-estado-com-redux/03-usando-o-redux-no-react-pratica/exercise-forms-redux/src/redux/reducers/reducer.js
@@ -0,0 +1,14 @@
+const INITIAL_STATE = {
+  state: '',
+}
+
+const infoReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'INFO_ACTION':
+      return { state }
+    default:
+      return state;
+  }
+}
+
+export default infoReducer;
