3d429ca8e519d8988c873279bb30cde7b322e664
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-03-03 15:48:53 -0300

    15.5 learning-exercises update

--- m2-front-end/bloco-15-gerenciamento-de-estado-com-redux/05-testes-em-react-redux/counter-app/src/App.test.js
+++ m2-front-end/bloco-15-gerenciamento-de-estado-com-redux/05-testes-em-react-redux/counter-app/src/App.test.js
@@ -4,6 +4,7 @@ import { render, cleanup } from '@testing-library/react';
 import App from './App';
 import { createStore, combineReducers } from 'redux';
 import clickReducer from './reducers';
+import userEvent from '@testing-library/user-event'
 
 const renderWithRedux = (
   component,
@@ -23,5 +24,30 @@ describe('testing clicks', () => {
 
     expect(buttonAdicionar).toBeInTheDocument();
     expect(queryByText('0')).toBeInTheDocument();
+  });
+  test('a click in a button should increment the value of clicks', () => {
+    const { queryByText } = renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 }}});
+
+    expect(queryByText('5')).toBeInTheDocument();
+  });
+  test('if a button click works', () => {
+    const { queryByText } = renderWithRedux(<App />);
+    const buttonAdicionar = queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(queryByText('0')).toBeInTheDocument();
+
+    userEvent.click(buttonAdicionar);
+    expect(queryByText('1')).toBeInTheDocument();
+  });
+  test('if the initial value of the counter is 10 and a button click still works', () => {
+    const { queryByText } = renderWithRedux(<App />, { initialState: { clickReducer: { counter: 10 }}});
+    const buttonAdicionar = queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(queryByText('10')).toBeInTheDocument();
+
+    userEvent.click(buttonAdicionar);
+    expect(queryByText('11')).toBeInTheDocument();
   });
 });
