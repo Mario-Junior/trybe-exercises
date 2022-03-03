5d652419f2a1ce099ed870eb08a2a4b14a40b1f0
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-03-03 15:02:12 -0300

    15.5 learning-exercises update

---
+++ m2-front-end/bloco-15-gerenciamento-de-estado-com-redux/05-testes-em-react-redux/counter-app/src/store/index.js
@@ -0,0 +1,9 @@
+// src/store/index.js
+import { createStore, combineReducers } from 'redux';
+import clickReducer from '../reducers';
+
+const rootReducer = combineReducers({ clickReducer });
+
+const store = createStore(rootReducer);
+
+export default store;
