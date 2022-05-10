e21f50d109d2f50be48a217f1185e6186183006f
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-05-10 08:40:17 -0300

    feat: 22.2 learning exercises

---
+++ m3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/02-node-js-fluxo-assincrono/io-local/readFilePromise.js
@@ -0,0 +1,13 @@
+const fs = require('fs').promises;
+
+const nomeDoArquivo = 'meu-arquivo.txt';
+
+fs.readFile(nomeDoArquivo, 'utf8')
+  .then((data) => {
+    console.log(`Conteúdo do arquivo:\n${data}`);
+  })
+  .catch((err) => {
+    console.error(`Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
+    process.exit(1); // Encerra a execução do script e informa ao sistema operacional que houve um erro com código
+  });
+  
