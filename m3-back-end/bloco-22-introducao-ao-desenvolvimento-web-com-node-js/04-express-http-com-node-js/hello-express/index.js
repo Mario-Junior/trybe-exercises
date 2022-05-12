ca3faf832a4e9323eae3d1ff3fd5a77bec4f4bb2
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-05-12 15:50:56 -0300

    feat: 22.4 learning exercises update

---
+++ m3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/04-express-http-com-node-js/hello-express/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+// 1. Cria uma nova aplicação Express;
+const app = express();
+
+// 4. Trata a requisição com método GET no caminho /hello, envia o status HTTP 200 que significa OK e a mensagem 'Hello World!'
+const handleHelloWorldRequest = (_req, res) => {
+  res.status(200).send('Hello World!');  
+};
+
+// 2. Diz ao Express que, quando uma requisição com método GET for recebida no caminho /hello, a função handleHelloWorldRequest deve ser chamada;
+app.get('/hello', handleHelloWorldRequest);
+
+// 3. Pede ao Express que crie um servidor HTTP e escute por requisições na porta 3001;
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
