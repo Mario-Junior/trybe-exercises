660faaea27502a0bc21151efa10793c363641ef0
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-05-13 09:11:22 -0300

    feat: 22.4 learning exercises update

--- m3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/04-express-http-com-node-js/recipes/index.js
+++ m3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/04-express-http-com-node-js/recipes/index.js
@@ -12,8 +12,21 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
-app.get('/recipes', function (req, res) {
-  res.json(recipes);
+app.get('/recipes', function (_req, res) {
+  res.json(recipes.sort((a, b) => a.name.localeCompare(b.name)));
+});
+
+const drinks = [
+	{ id: 1, name: 'Refrigerante Lata', price: 5.0 },
+	{ id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+	{ id: 3, name: 'Suco 300ml', price: 4.0 },
+	{ id: 4, name: 'Suco 1l', price: 10.0 },
+	{ id: 5, name: 'Cerveja Lata', price: 4.5 },
+	{ id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+
+app.get('/drinks', function (_req, res) {
+  res.json(drinks.sort((a, b) => a.name.localeCompare(b.name)));
 });
 
 app.listen(3001, () => {
