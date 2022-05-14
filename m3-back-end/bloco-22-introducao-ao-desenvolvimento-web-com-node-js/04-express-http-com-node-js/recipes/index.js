6d630b6063bf252d5a6fe17ab9641d73010beb15
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-05-14 09:52:32 -0300

    feat: 22.4 learning exercises update

--- m3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/04-express-http-com-node-js/recipes/index.js
+++ m3-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/04-express-http-com-node-js/recipes/index.js
@@ -8,12 +8,29 @@ app.use(cors());
 
 const recipes = [
   { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
-  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 30.0, waitTime: 25 },
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
 app.get('/recipes', function (_req, res) {
   res.json(recipes.sort((a, b) => a.name.localeCompare(b.name)));
+});
+
+app.get('/recipes/search', function (req, res) {
+  const { name, minPrice, maxPrice } = req.query;
+  const filteredRecipes = recipes
+    .filter((recipe) => recipe.name.toLowerCase().includes(name.toLowerCase())
+  && recipe.price >= Number(minPrice)
+  && recipe.price < parseInt(maxPrice));
+  res.status(200).json(filteredRecipes);
+});
+
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = recipes.find((recipe) => recipe.id === parseInt(id));
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
+  
+  res.status(200).json(recipe);
 });
 
 const drinks = [
@@ -27,6 +44,14 @@ const drinks = [
 
 app.get('/drinks', function (_req, res) {
   res.json(drinks.sort((a, b) => a.name.localeCompare(b.name)));
+});
+
+app.get('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const drink = drinks.find((drink) => drink.id === Number(id));
+
+  if(!drink) return res.status(404).json({message: 'No Orange Juice here!'});
+  res.status(200).json(drink);
 });
 
 app.listen(3001, () => {
