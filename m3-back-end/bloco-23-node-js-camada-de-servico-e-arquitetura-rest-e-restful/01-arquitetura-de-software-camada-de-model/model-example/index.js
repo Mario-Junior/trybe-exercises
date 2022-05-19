0c135bc09bdadd9d585524e1ecc6016d23acfa87
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-05-19 09:34:16 -0300

    feat: 23.1 learning exercises update

--- m3-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/01-arquitetura-de-software-camada-de-model/model-example/index.js
+++ m3-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/01-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
+const Author = require('./models/Author');
+
 const app = express();
+
+app.get('/authors', async (_req, res) => {
+  const authors = await Author.getAll();
+
+  res.status(200).json(authors);
+})
 
 const PORT = process.env.PORT || 3000;
 
