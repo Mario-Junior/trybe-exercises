504484cc70cb1f1b2528d3a614580c6d0490f420
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-05-19 14:53:47 -0300

    feat: 23.1 learning exercises update

--- m3-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/01-arquitetura-de-software-camada-de-model/model-example/index.js
+++ m3-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/01-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -4,6 +4,19 @@ const Author = require('./models/Author');
 const Book = require('./models/Book');
 
 const app = express();
+app.use(express.json());
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Author.isValid(first_name, middle_name, last_name)) {
+    return res.status(400).json({ message: 'Invalid data!'});
+  }
+
+  await Author.create(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'Author successfully created!'});
+});
 
 app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
--- m3-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/01-arquitetura-de-software-camada-de-model/model-example/models/Author.js
+++ m3-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/01-arquitetura-de-software-camada-de-model/model-example/models/Author.js
@@ -51,7 +51,22 @@ const findById = async (id) => {
   });
 };
 
+const isValid = (firstName, middleName, lastName) => {
+  if (!firstName || typeof firstName !== 'string') return false;
+  if (!lastName || typeof lastName !== 'string') return false;
+  if (middleName && typeof middleName !== 'string') return false;
+
+  return true;
+};
+
+const create = async (firstName, middleName, lastName) => connection.execute(
+  'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?, ?, ?)',
+  [firstName, middleName, lastName],
+);
+
 module.exports = {
   getAll,
   findById,
+  isValid,
+  create,
 };
