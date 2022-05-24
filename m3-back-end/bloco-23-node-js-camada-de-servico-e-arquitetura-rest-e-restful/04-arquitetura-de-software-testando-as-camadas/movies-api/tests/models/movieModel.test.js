f7f29cbddd502f6bd46ee7deac31a71b18642f3a
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-05-23 21:44:15 -0300

    feat: 23.4 learning exercises tests update

---
+++ m3-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/04-arquitetura-de-software-testando-as-camadas/movies-api/tests/models/movieModel.test.js
@@ -0,0 +1,29 @@
+const { expect } = require('chai');
+
+// Como ainda não há implementação, fixo um objeto simulando os métodos que desenvolverei, porém, eles não terão nenhum comportamento
+const MoviesModel = {
+  create: () => {}
+};
+
+describe('Insere um novo filme no BD', () => {
+  const payloadMovie = {
+    title: 'Example Movie',
+    directedBy: 'Jane Dow',
+    releaseYear: 1999,
+  };
+
+  describe('Quando é inserido com sucesso', () => {
+
+    it('retorna um objeto', async () => {
+      const response = await MoviesModel.create(payloadMovie);
+
+      expect(response).to.be.a('object');
+    });
+
+    it('tal objeto possui o "id" do novo filme inserido', async () => {
+      const response = await MoviesModel.create(payloadMovie);
+
+      expect(response).to.have.a.property('id');
+    });
+  });
+});
