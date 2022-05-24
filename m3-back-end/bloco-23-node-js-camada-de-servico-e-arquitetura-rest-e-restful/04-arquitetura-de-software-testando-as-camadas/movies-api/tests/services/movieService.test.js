74e3f203ab8637bc006e33fd21fa031a4ca5f0c1
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-05-24 09:20:53 -0300

    feat: 23.4 learning exercises update
    - tests implementation in the service layer

---
+++ m3-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/04-arquitetura-de-software-testando-as-camadas/movies-api/tests/services/movieService.test.js
@@ -0,0 +1,45 @@
+const { expect } = require('chai');
+
+const MoviesService = {
+  create: () => {},
+};
+
+// Preciso validar se recebo todos os campos necessários à operação. Como trata-se de regra de negócio, valido na camada de serviços.
+
+describe('Insere um novo filme no BD', () => {
+  describe('Quando o payload informado não é válido', () => {
+    const payloadMovie = {};
+
+    it('retorna um boolean', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.a('boolean');
+    });
+
+    it('o boolean contém "false"', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.equal(false);
+    });
+  });
+
+  describe('Quando é inserido com sucesso', () => {
+    const payloadMovie = {
+      title: 'Example Movie',
+      directedBy: 'Jane Dow',
+      releaseYear: 1999,
+    };
+
+    it('retorna um objeto', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.a('object');
+    });
+
+    it('tal objeto possui o "id" do novo filme inserido', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.have.a.property('id');
+    });
+  });
+});
