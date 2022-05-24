cf4eb4e1142f73fb61051574d269ad1049f70136
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-05-23 22:02:11 -0300

    feat: 23.4 learning exercises tests update

--- m3-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/04-arquitetura-de-software-testando-as-camadas/movies-api/tests/models/movieModel.test.js
+++ m3-back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/04-arquitetura-de-software-testando-as-camadas/movies-api/tests/models/movieModel.test.js
@@ -1,9 +1,14 @@
+const sinon = require('sinon');
 const { expect } = require('chai');
 
 // Como ainda não há implementação, fixo um objeto simulando os métodos que desenvolverei, porém, eles não terão nenhum comportamento
-const MoviesModel = {
-  create: () => {}
-};
+// const MoviesModel = {
+//   create: () => {}
+// };
+
+// Edito o teste para utilizar a minha implementação em models
+const MoviesModel = require('../../models/movieModel');
+const connection = require('../../models/connection');
 
 describe('Insere um novo filme no BD', () => {
   const payloadMovie = {
@@ -11,6 +16,17 @@ describe('Insere um novo filme no BD', () => {
     directedBy: 'Jane Dow',
     releaseYear: 1999,
   };
+
+  before(async () => {
+    const execute = [{ insertId: 1 }]; // retorno esperado no teste
+
+    sinon.stub(connection, 'execute').resolves(execute);
+  });
+
+  // Restauro a função 'execute' original após os testes
+  after(async () => {
+    connection.execute.restore();
+  });
 
   describe('Quando é inserido com sucesso', () => {
 
