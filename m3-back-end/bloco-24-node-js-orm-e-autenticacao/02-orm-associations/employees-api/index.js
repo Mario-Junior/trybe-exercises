218d14d0277e569d3d36593fe47d9f96f4cdac5c
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-06-07 13:43:06 -0300

    feat: add of new route "get by id" in application

--- m3-back-end/bloco-24-node-js-orm-e-autenticacao/02-orm-associations/employees-api/index.js
+++ m3-back-end/bloco-24-node-js-orm-e-autenticacao/02-orm-associations/employees-api/index.js
@@ -19,6 +19,24 @@ app.get('/employees', async (_req, res) => {
   };
 });
 
+app.get('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await Employee.findOne({
+      where: { id },
+      include: [{ model: Address, as: 'addresses' }],
+    });
+
+    if (! employee)
+      return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+    return res.status(200).json(employee);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  };
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
 
