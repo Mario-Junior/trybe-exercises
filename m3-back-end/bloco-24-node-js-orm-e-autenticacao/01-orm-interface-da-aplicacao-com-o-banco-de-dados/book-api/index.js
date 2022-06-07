a3c660af9d0da23a0e4344d9025157b7096ae2e2
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-06-06 22:23:15 -0300

    feat: uploading de application with express

---
+++ m3-back-end/bloco-24-node-js-orm-e-autenticacao/01-orm-interface-da-aplicacao-com-o-banco-de-dados/book-api/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(
+  `App listening on PORT ${PORT}`
+));
