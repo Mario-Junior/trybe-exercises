5c15aa4e5630137e36e2939c9807706562c2f2da
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-06-07 15:43:30 -0300

    feat: book model created

---
+++ m3-back-end/bloco-24-node-js-orm-e-autenticacao/02-orm-associations/users-books-api/migrations/20220607184209-create-book.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      bookId: {
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      releaseYear: {
+        type: Sequelize.INTEGER
+      },
+      numberPages: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Books');
+  }
+};
