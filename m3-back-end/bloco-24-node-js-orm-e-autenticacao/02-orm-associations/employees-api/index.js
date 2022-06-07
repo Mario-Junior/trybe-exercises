68084cbe9809706511eeabe61de86ae1c0148b7d
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-06-07 15:24:19 -0300

    feat: testing lazy loading

--- m3-back-end/bloco-24-node-js-orm-e-autenticacao/02-orm-associations/employees-api/index.js
+++ m3-back-end/bloco-24-node-js-orm-e-autenticacao/02-orm-associations/employees-api/index.js
@@ -22,13 +22,22 @@ app.get('/employees', async (_req, res) => {
 app.get('/employees/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const employee = await Employee.findOne({
-      where: { id },
-      include: [{ model: Address, as: 'addresses', attributes: { exclude: ['number'] }}],
-    });
+    // formato Eager Loading (comentar/descomentar para usar!)
+    // const employee = await Employee.findOne({
+    //   where: { id },
+    //   include: [{ model: Address, as: 'addresses', attributes: { exclude: ['number'] }}],
+    // });
 
-    if (! employee)
+    // formato Lazy Loading (comentar/descomentar para usar!)
+    const employee = await Employee.findOne({ where: { id } });
+
+    if (!employee)
       return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+    if (req.query.includeAddresses === 'true') {
+      const addresses = await Address.findAll({ where: { employeeId: id } });
+      return res.status(200).json({ employee, addresses });
+    }
 
     return res.status(200).json(employee);
   } catch (err) {
