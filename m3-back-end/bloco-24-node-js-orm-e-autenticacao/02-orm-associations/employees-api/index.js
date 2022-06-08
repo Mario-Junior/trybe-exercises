1885587bab2e9848ab9653d9cc767e2ced6f0052
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-06-07 22:11:24 -0300

    feat: post method created with unmanaged transaction

--- m3-back-end/bloco-24-node-js-orm-e-autenticacao/02-orm-associations/employees-api/index.js
+++ m3-back-end/bloco-24-node-js-orm-e-autenticacao/02-orm-associations/employees-api/index.js
@@ -3,8 +3,46 @@
 // index.js
 const express = require('express');
 const { Address, Employee } = require('./models');
+const Sequelize = require('sequelize'); // importação do sequelize
+const config = require('./config/config'); // importação do config
 
 const app = express();
+
+app.use(express.json());
+
+const sequelize = new Sequelize(config.development); // instanciamento do Sequelize com config.development
+
+app.post('/employees', async (req, res) => {
+  // Iniciando a transação
+  const tr = await sequelize.transaction();
+
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+
+    // Executo as operações
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: tr },
+      );
+
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: tr },
+      );
+
+    // Se chegou até essa linha, quer dizer que nenhum erro ocorreu
+    // Com isso, posso finalizar a transação usando a função 'commit'
+    await tr.commit();
+
+    return res.status(201).json({ message: 'Cadastro efetuado com sucesso' });
+  } catch (err) {
+    // Se entrou aqui no catch, é porque alguma operação falhou
+    // Nesse caso, o sequelize reverterá as operações anteriores com a função rollback
+    await tr.rollback(); 
+    console.log(err.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  };
+});
 
 app.get('/employees', async (_req, res) => {
   try {
