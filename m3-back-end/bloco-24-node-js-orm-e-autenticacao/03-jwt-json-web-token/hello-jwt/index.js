7240406f76964dac9e8dedf1cec791c45f4eb4fc
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-06-09 00:20:53 -0300

    feat: practice exercise 1 - endpoint post/login creation

--- m3-back-end/bloco-24-node-js-orm-e-autenticacao/03-jwt-json-web-token/hello-jwt/index.js
+++ m3-back-end/bloco-24-node-js-orm-e-autenticacao/03-jwt-json-web-token/hello-jwt/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const { PORT } = process.env;
 
@@ -18,8 +18,13 @@ app.use(
   }),
 );
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/login', controllers.login);
 
 app.get('/ping', controllers.ping);
 
