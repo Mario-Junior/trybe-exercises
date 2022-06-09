87b0278f0fe9dfd46e1710ef9f42cb38f6372f0f
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-06-09 11:04:51 -0300

    test: creating new user and validating it

--- m3-back-end/bloco-24-node-js-orm-e-autenticacao/04-testando-apis-com-testes-de-integracao/nodejs-jwt-base-project/tests/createUsers.test.js
+++ m3-back-end/bloco-24-node-js-orm-e-autenticacao/04-testando-apis-com-testes-de-integracao/nodejs-jwt-base-project/tests/createUsers.test.js
@@ -17,12 +17,14 @@ const { expect } = chai;
 describe('Rota /api/users', () => {
   before(() => {
     sinon.stub(User, 'findAll').callsFake(userMock.findAll);
+    sinon.stub(User, 'create').callsFake(userMock.create);
   });
 
   after(() => {
     User.findAll.restore();
+    User.create.restore();
   });
-  
+
   describe('Consulta a lista de pessoas usuárias', () => {
     let response;
 
@@ -35,6 +37,53 @@ describe('Rota /api/users', () => {
     });
     it('Essa requisição deve retornar código de status 200', () => {
       expect(response).to.have.status(200);
+    });
+  });
+
+  describe('Insere novo registro', () => {
+    let createRequest = {};
+    let firstUserList = [];
+    let secondUserList = [];
+    const newUser = {
+      username: 'tryber',
+      password: 'trybe@123',
+    };
+
+    before(async () => {
+      firstUserList = await chai.request(server).get('/api/users').then(({ body }) => body);
+      createRequest = await chai.request(server).post('/api/users').send(newUser);
+      secondUserList = await chai.request(server).get('/api/users').then(({ body }) => body);
+    });
+
+    it('firstUserList: a primeira requisição GET para a rota deve retornar 2 registros', () => {
+      expect(firstUserList).to.have.length(2);
+    });
+
+    it('createRequest: a requisição POST para a rota retorna o código de status 201', () => {
+      expect(createRequest).to.have.status(201);
+    });
+
+    it('createRequest: a requisição deve retornar um objeto no corpo da resposta', () => {
+      expect(createRequest.body).to.be.an('object');
+    });
+
+    it('createRequest: o objeto possui a propriedade "message"', () => {
+      expect(createRequest.body).to.have.property('message');
+    });
+
+    it('createRequest: a propriedade "message" possui o texto "Novo usuário criado com sucesso"',
+      () => {
+        expect(createRequest.body.message).to.be.equal('Novo usuário criado com sucesso');
+    });
+
+    it('secondUserList: a segunda requisição GET para rota deve retornar, por tanto, 3 registros',
+      () => {
+        expect(secondUserList).to.have.length(3);
+    });
+
+    it('secondUserList: o registro criado deve corresponder ao enviado na requisição POST',
+      () => {
+        expect(secondUserList[2]).to.contain(newUser);
     });
   });
 });
