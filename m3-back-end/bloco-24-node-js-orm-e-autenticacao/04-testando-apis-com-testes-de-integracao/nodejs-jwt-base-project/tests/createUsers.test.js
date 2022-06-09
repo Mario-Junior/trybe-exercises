83b023b385575596e6c763704f9e9684e8f46710
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-06-09 10:54:18 -0300

    test: query the list of all users

--- m3-back-end/bloco-24-node-js-orm-e-autenticacao/04-testando-apis-com-testes-de-integracao/nodejs-jwt-base-project/tests/createUsers.test.js
+++ m3-back-end/bloco-24-node-js-orm-e-autenticacao/04-testando-apis-com-testes-de-integracao/nodejs-jwt-base-project/tests/createUsers.test.js
@@ -1,16 +1,33 @@
 const chai = require('chai');
+const sinon = require('sinon');
 const chaiHttp = require('chai-http');
+
+const server = require('../api/app');
+
+// Importação do modelo original, contido em `models`, a partir da raiz
+const { User } = require('../models');
+
+// Importação do mock utilizado nesse contexto
+const { User: userMock } = require('./mock/models');
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Rota /api/users', () => {
+  before(() => {
+    sinon.stub(User, 'findAll').callsFake(userMock.findAll);
+  });
+
+  after(() => {
+    User.findAll.restore();
+  });
+  
   describe('Consulta a lista de pessoas usuárias', () => {
     let response;
 
     before(async () => {
-      response = await minhaRequisicao();
+      response = await chai.request(server).get('/api/users');
     });
 
     it('A requisição GET para a rota traz a lista inicial contendo dois registros', () => {
