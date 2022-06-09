01fe000b08a8f986471fbe5af1a999db4a173288
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-06-09 10:31:40 -0300

    test: modeling the first case for GET route

---
+++ m3-back-end/bloco-24-node-js-orm-e-autenticacao/04-testando-apis-com-testes-de-integracao/nodejs-jwt-base-project/tests/createUsers.test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+
+const { expect } = chai;
+
+describe('Rota /api/users', () => {
+  describe('Consulta a lista de pessoas usuárias', () => {
+    let response;
+
+    before(async () => {
+      response = await minhaRequisicao();
+    });
+
+    it('A requisição GET para a rota traz a lista inicial contendo dois registros', () => {
+      expect(response.body).to.have.length(2);
+    });
+    it('Essa requisição deve retornar código de status 200', () => {
+      expect(response).to.have.status(200);
+    });
+  });
+});
