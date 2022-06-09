230082e5e9787b63510e6f694296c092d6a6d7c6
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-06-09 10:42:32 -0300

    test: implementation of chaiHttp

--- m3-back-end/bloco-24-node-js-orm-e-autenticacao/04-testando-apis-com-testes-de-integracao/nodejs-jwt-base-project/api/server.js
+++ m3-back-end/bloco-24-node-js-orm-e-autenticacao/04-testando-apis-com-testes-de-integracao/nodejs-jwt-base-project/api/server.js
@@ -1,16 +1,19 @@
+// Para os testes, toda a estrutura anterior (aqui comentada) foi para o app.js
+/* 
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 // Importo a função que valida se o usuário está ou não autenticado
 const validateJWT = require('./auth/validateJWT');
 
-const PORT = process.env.PORT || 8080;
-
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const apiRoutes = express.Router();
 
@@ -20,5 +23,9 @@ apiRoutes.get('/api/users', routes.getUsers);
 apiRoutes.post('/api/login', routes.login);
 
 app.use(apiRoutes);
+*/
+const app = require('./app');
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Conectado na porta ${PORT}`));
--- m3-back-end/bloco-24-node-js-orm-e-autenticacao/04-testando-apis-com-testes-de-integracao/nodejs-jwt-base-project/tests/createUsers.test.js
+++ m3-back-end/bloco-24-node-js-orm-e-autenticacao/04-testando-apis-com-testes-de-integracao/nodejs-jwt-base-project/tests/createUsers.test.js
@@ -1,4 +1,7 @@
 const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
 
 const { expect } = chai;
 
