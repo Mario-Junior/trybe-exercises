4ba562cfc2bb85340b75137eed059ee08d5e459b
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-07-05 10:14:15 -0300

    feat: 26.1 enum exercises update

---
+++ m3-back-end/bloco-26-typescript/01-introducao-ao-typescript/enum-exercises/index.js
@@ -0,0 +1,24 @@
+var weekDays;
+(function (weekDays) {
+    weekDays[weekDays["Sunday"] = 1] = "Sunday";
+    weekDays[weekDays["Monday"] = 2] = "Monday";
+    weekDays[weekDays["Tuesday"] = 3] = "Tuesday";
+    weekDays[weekDays["Wednesday"] = 4] = "Wednesday";
+    weekDays[weekDays["Thursday"] = 5] = "Thursday";
+    weekDays[weekDays["Friday"] = 6] = "Friday";
+    weekDays[weekDays["Saturday"] = 7] = "Saturday";
+})(weekDays || (weekDays = {}));
+;
+console.log(weekDays);
+var rainbowColors;
+(function (rainbowColors) {
+    rainbowColors["red"] = "vermelha";
+    rainbowColors["orange"] = "laranja";
+    rainbowColors["yellow"] = "amarelo";
+    rainbowColors["green"] = "verde";
+    rainbowColors["blue"] = "azul";
+    rainbowColors["indigo"] = "anil";
+    rainbowColors["purple"] = "roxo";
+})(rainbowColors || (rainbowColors = {}));
+;
+console.log(rainbowColors);
---
+++ m3-back-end/bloco-26-typescript/01-introducao-ao-typescript/enum-exercises/index.ts
@@ -0,0 +1,21 @@
+enum weekDays {
+  Sunday = 1,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+};
+console.log(weekDays);
+
+enum rainbowColors {
+  red = 'vermelha',
+  orange = 'laranja',
+  yellow = 'amarelo',
+  green = 'verde',
+  blue = 'azul',
+  indigo = 'anil',
+  purple = 'roxo',
+};
+console.log(rainbowColors);
