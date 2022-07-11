8ad096fd697143a8071cd9d181d08525589d79f6
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-07-11 08:49:08 -0300

    feat: 27.1 learning exercises update

--- m3-back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/01-introducao-a-orientacao-a-objetos/learning-exercises/personClass.ts
+++ m3-back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/01-introducao-a-orientacao-a-objetos/learning-exercises/personClass.ts
@@ -1,8 +1,12 @@
+// palavra reservada class, seguida do 'NomeDaClasse' e um {...} (par de chaves)
 class Person {
+  // criação de atributos => 'nomeAtributo: tipo;'
   name: string;
   height: number;
   weight: number;
 
+  // criação de métodos => 'nomeDoMetodo() {...}'
+  // ATENÇÃO ao método construtor
   constructor(n: string, h: number, w: number) {
     console.log(`Creating person ${n}`);
     this.name = n;
@@ -10,11 +14,13 @@ class Person {
     this.weight = w;
   }
 
+  // criação de métodos => 'nomeDoMetodo() {...}'
   sleep() {
     console.log(`${this.name}: zzzzzzz`);
   }
 }
 
+// criação de objetos (neste caso de uma mesma classe), via chamada do método constructor, com a palavra reservada 'new'
 const p1 = new Person('Adrielle', 166, 54);
 const p2 = new Person('Mário Júnior', 178, 83);
 console.log(p1.name, p1.height, p1.weight);
