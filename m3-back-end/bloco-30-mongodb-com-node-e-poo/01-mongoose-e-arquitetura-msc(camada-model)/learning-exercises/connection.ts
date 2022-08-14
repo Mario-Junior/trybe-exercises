ffb62779aba41e3c4b924bd6083b77079bddb516
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-08-13 21:54:00 -0300

    feat: 30.1 learning exercises update

--- m3-back-end/bloco-30-mongodb-com-node-e-poo/01-mongoose-e-arquitetura-msc(camada-model)/learning-exercises/connection.ts
+++ m3-back-end/bloco-30-mongodb-com-node-e-poo/01-mongoose-e-arquitetura-msc(camada-model)/learning-exercises/connection.ts
@@ -1,3 +1,18 @@
 import { connect } from 'mongoose';
 
 connect('mongodb://localhost:27017/mongodb');
+
+// DESCOMENTAR para entendimento!
+// Exemplo com URI:
+// connect('mongodb://username:password@host:port/nomeDaMinhaBaseDeDados');
+
+// Exemplos com options:
+// connect('mongodb://localhost:27017/nomeDaMinhaBaseDeDados', { user: 'user', pass: 'password' });
+// OU
+// const options = {
+//   user: 'user', // Usuário do banco de dados.
+//   pass: 'password' // senha do usuário do banco de dados.
+//   autoIndex: false, // Não cria index para cada inserção de dado no banco.
+//   dbName: 'glassesStore', // Define qual banco de dados vou utilizar.
+// };
+// connect('mongodb://localhost:27017/nomeDaMinhaBaseDeDados', options);
