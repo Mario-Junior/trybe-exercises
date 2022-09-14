53c9c89f0bd45612596c8fc7763fd6bc301ca29e
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-14 10:28:18 -0300

    feat: learning python exercises

--- m4-ciencia-da-computacao/bloco-33-introducao-a-python/01-aprendendo-python/learning-exercises/geometric_area.py
+++ m4-ciencia-da-computacao/bloco-33-introducao-a-python/01-aprendendo-python/learning-exercises/geometric_area.py
@@ -2,21 +2,22 @@ PI = 3.14
 
 
 def square(side):
-    "Calculate area of square"
+    """Calculate area of square"""
     return side * side
 
 
 def rectangle(length, width):
-    "Calculate area of rectangle"
+    """Calculate area of rectangle"""
     return length * width
 
 
 def circle(radius):
-    "Calculate area of circle"
+    """Calculate area of circle"""
     return PI * radius ** 2
 
 
 if __name__ == "__main__":
-    print("Área do quadrado:", square(12))
-    print("Área do retângulo:", rectangle(3, 4))
-    print("Área do círculo:", circle(6))
+    print(f'{square.__doc__}:', square(12))
+    print(f'{rectangle.__doc__}:', rectangle(3, 4))
+    print(f'{circle.__doc__}:', circle(6))
+    # print(f'{square.__doc__}\n{rectangle.__doc__}\n{circle.__doc__}')
