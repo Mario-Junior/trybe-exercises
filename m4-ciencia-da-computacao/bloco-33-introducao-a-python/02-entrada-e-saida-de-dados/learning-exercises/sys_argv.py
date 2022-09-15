64300f0fbe78ae18d87f8af5e048711f68751a2a
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-15 11:27:44 -0300

    feat: data input|output learning exercises

--- m4-ciencia-da-computacao/bloco-33-introducao-a-python/02-entrada-e-saida-de-dados/learning-exercises/sys_argv.py
+++ m4-ciencia-da-computacao/bloco-33-introducao-a-python/02-entrada-e-saida-de-dados/learning-exercises/sys_argv.py
@@ -4,3 +4,7 @@ import sys
 if __name__ == "__main__":
     for argument in sys.argv:
         print("Received -> ", argument)
+
+
+err = "Erro modificado!"
+print(f'Erro aconteceu: {err}', file=sys.stderr)
