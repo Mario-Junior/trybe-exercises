98d885751556da96371547337d9159ea88052289
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-27 07:31:06 -0300

    feat: 34.3 decorator, observer, factory patterns exercises

--- m4-ciencia-da-computacao/bloco-34-padroes-de-projeto/03-padroes-decorator-observer-factory/learning-exercises/factory.py
+++ m4-ciencia-da-computacao/bloco-34-padroes-de-projeto/03-padroes-decorator-observer-factory/learning-exercises/factory.py
@@ -42,3 +42,44 @@ class ItemSorvete(Item):
 class ItemFritas(Item):
     def __repr__(self):
         return "Fritas"
+
+
+class ComboTudo(Combo):
+    def criar_combo(self):
+        self.adicionar_itens(ItemHamburger())
+        self.adicionar_itens(ItemSorvete())
+        self.adicionar_itens(ItemFritas())
+        self.adicionar_itens(ItemRefrigerante())
+
+
+class ComboFeliz(Combo):
+    def criar_combo(self):
+        self.adicionar_itens(ItemHamburger())
+        self.adicionar_itens(ItemFritas())
+        self.adicionar_itens(ItemRefrigerante())
+
+
+class ComboGelado(Combo):
+    def criar_combo(self):
+        self.adicionar_itens(ItemHamburger())
+        self.adicionar_itens(ItemSorvete())
+
+
+class ComboFritas(Combo):
+    def criar_combo(self):
+        self.adicionar_itens(ItemHamburger())
+        self.adicionar_itens(ItemFritas())
+
+
+if __name__ == "__main__":
+    combo_escolhido = input(
+        "Olá, qual seu pedido? "
+        "[ComboTudo, ComboFeliz, ComboGelado, ComboFritas]: "
+    )
+
+    # Para transformar string em código executável basta usar o método eval()
+    # Equivalente a chamar ComboTudo(), ComboFeliz(), ComboGelado()
+    combo = eval(combo_escolhido)()
+
+    print(f"\nCriando o combo {type(combo).__name__}.")
+    print(f"Combo fabricado com os seguintes itens: {combo.exibe_itens()}")
