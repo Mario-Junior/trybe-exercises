7daed876557a188205e19adc3594965e66d73429
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-27 07:16:26 -0300

    feat: 34.3 decorator, observer, factory patterns exercises

---
+++ m4-ciencia-da-computacao/bloco-34-padroes-de-projeto/03-padroes-decorator-observer-factory/learning-exercises/observer.py
@@ -0,0 +1,19 @@
+class Perfil:
+    def __init__(self):
+        self.__sistemas_de_notificacao = []
+        self.__new_post = None
+
+    def adicionar_sistema_de_notificacao(self, sistema):
+        self.__sistemas_de_notificacao.append(sistema)
+
+    def notificar_todos(self):
+        for sistema in self.__sistemas_de_notificacao:
+            sistema.notificar()
+
+    def adicionar_post(self, post):
+        self.__new_post = post
+        self.mostrar_post()
+        self.notificar_todos()
+
+    def mostrar_post(self):
+        print(f"\nPost: {self.__new_post}\n")
