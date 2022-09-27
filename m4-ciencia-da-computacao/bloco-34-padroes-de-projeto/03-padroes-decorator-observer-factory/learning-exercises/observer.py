1251cf1b9cd999993c5a166f3f844fa0414b1d95
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-27 07:18:38 -0300

    feat: 34.3 decorator, observer, factory patterns exercises

--- m4-ciencia-da-computacao/bloco-34-padroes-de-projeto/03-padroes-decorator-observer-factory/learning-exercises/observer.py
+++ m4-ciencia-da-computacao/bloco-34-padroes-de-projeto/03-padroes-decorator-observer-factory/learning-exercises/observer.py
@@ -60,3 +60,17 @@ class NotificadorEmail(Notificador):
 
     def notificar(self):
         print(f"Disparando Email's para: {self.seguidores}")
+
+
+# Cliente
+if __name__ == "__main__":
+    seguidores_mensagem = ["Carlos", "Claudia", "Marcia", "Rodolfo"]
+    seguidores_push_notification = ["Carlos"]
+    seguidores_email = ["Claudia", "Marcia"]
+
+    meuPerfil = Perfil()
+    NotificadorMensagem(meuPerfil, seguidores_mensagem)
+    NotificadorPushNotification(meuPerfil, seguidores_push_notification)
+    NotificadorEmail(meuPerfil, seguidores_email)
+
+    meuPerfil.adicionar_post("Olá universo da programação!")
