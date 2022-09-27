00627eb42e5b6f9b13e886b0e2c68a948f86d20d
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-27 07:17:29 -0300

    feat: 34.3 decorator, observer, factory patterns exercises

--- m4-ciencia-da-computacao/bloco-34-padroes-de-projeto/03-padroes-decorator-observer-factory/learning-exercises/observer.py
+++ m4-ciencia-da-computacao/bloco-34-padroes-de-projeto/03-padroes-decorator-observer-factory/learning-exercises/observer.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractmethod
+
+
 class Perfil:
     def __init__(self):
         self.__sistemas_de_notificacao = []
@@ -17,3 +20,43 @@ class Perfil:
 
     def mostrar_post(self):
         print(f"\nPost: {self.__new_post}\n")
+
+
+# Interface Observer
+class Notificador(ABC):
+    @abstractmethod
+    def notificar(self):
+        pass
+
+
+# Observador Mensagem
+class NotificadorMensagem(Notificador):
+    def __init__(self, perfil, seguidores):
+        self.perfil = perfil
+        self.seguidores = seguidores
+        self.perfil.adicionar_sistema_de_notificacao(self)
+
+    def notificar(self):
+        print(f"Notificando via Mensagens: {self.seguidores}")
+
+
+# Observador Push Notification
+class NotificadorPushNotification(Notificador):
+    def __init__(self, perfil, seguidores):
+        self.perfil = perfil
+        self.seguidores = seguidores
+        self.perfil.adicionar_sistema_de_notificacao(self)
+
+    def notificar(self):
+        print(f"Disparando Push Notification para: {self.seguidores}")
+
+
+# Observador Email
+class NotificadorEmail(Notificador):
+    def __init__(self, perfil, seguidores):
+        self.perfil = perfil
+        self.seguidores = seguidores
+        self.perfil.adicionar_sistema_de_notificacao(self)
+
+    def notificar(self):
+        print(f"Disparando Email's para: {self.seguidores}")
