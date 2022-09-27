90de3f5c897f4c9ae41c459bdda95e7c096b4c6a
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-27 07:42:53 -0300

    feat: 34.3 decorator, observer, factory patterns exercises

--- m4-ciencia-da-computacao/bloco-34-padroes-de-projeto/03-padroes-decorator-observer-factory/learning-exercises/factory.py
+++ m4-ciencia-da-computacao/bloco-34-padroes-de-projeto/03-padroes-decorator-observer-factory/learning-exercises/factory.py
@@ -83,3 +83,21 @@ if __name__ == "__main__":
 
     print(f"\nCriando o combo {type(combo).__name__}.")
     print(f"Combo fabricado com os seguintes itens: {combo.exibe_itens()}")
+
+
+# Dica: Em Python, é possível imprimir uma saída colorida.
+# Para isso, basta a string começar com um código da cor antes
+# e terminar com um código que indica o fim da formatação.
+ROSA = "\033[95m"
+ROXO = "\033[94m"
+AZUL = "\033[96m"
+VERDE = "\033[92m"
+LARANJA = "\033[93m"
+VERMELHO = "\033[91m"
+NEGRITO = "\033[1m"
+SUBLINHADO = "\033[4m"
+RESET = "\033[0m"
+
+print("\033[94mOlá Mundo!\033[0m")
+# ou
+print(f"{ROSA}Olá Mundo!{RESET}")
