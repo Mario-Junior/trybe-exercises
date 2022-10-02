8ddfd262075cb17631d713a1254cffce06554366
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-02 17:34:06 -0300

    feat: 35.2 data scraping exercises

---
+++ m4-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/02-raspagem-de-dados/learning-exercises/04paginated_resources.py
@@ -0,0 +1,18 @@
+import requests
+from parsel import Selector
+
+
+# Define a primeira página como próxima a ter seu conteúdo recuperado
+URL_BASE = "http://books.toscrape.com/catalogue/"
+next_page_url = 'page-1.html'
+while next_page_url:
+    # Busca o conteúdo da próxima página
+    response = requests.get(URL_BASE + next_page_url)
+    selector = Selector(text=response.text)
+    # Imprime os produtos de uma determinada página
+    for product in selector.css(".product_pod"):
+        title = product.css("h3 a::attr(title)").get()
+        price = product.css(".price_color::text").get()
+        print(title, price)
+    # Descobre qual é a próxima página
+    next_page_url = selector.css(".next a::attr(href)").get()
