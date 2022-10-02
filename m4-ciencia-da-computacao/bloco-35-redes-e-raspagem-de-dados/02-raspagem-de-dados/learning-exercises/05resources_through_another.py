70b1af56e00a5d10732b5f106da00054d3344850
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-02 17:41:58 -0300

    feat: 35.2 data scraping exercises

--- m4-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/02-raspagem-de-dados/learning-exercises/05resources_through_another.py
+++ m4-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/02-raspagem-de-dados/learning-exercises/05resources_through_another.py
@@ -3,13 +3,17 @@ from parsel import Selector
 
 URL_BASE = "http://books.toscrape.com/catalogue/"
 
-# Vamos testar com a primeira página
 response = requests.get(URL_BASE + "page-1.html")
 selector = Selector(text=response.text)
 
-# Recupera o atributo href do primeiro elemento que combine com o seletor
 href = selector.css(".product_pod h3 a::attr(href)").get()
-print(href)
+detail_page_url = URL_BASE + href
 
-# Para obter a url completa, basta adicionar a nossa URL_BASE
-print(URL_BASE + href)
+# baixa o conteúdo da página de detalhes
+detail_response = requests.get(detail_page_url)
+detail_selector = Selector(text=detail_response.text)
+
+# recupera a descrição do produto
+# ~ significa a tag irmã
+description = detail_selector.css("#product_description ~ p::text").get()
+print(description)
