4db94659df8b7c6c1b4c19911bff83163a7e3b74
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-02 16:48:37 -0300

    feat: 35.2 data scraping exercises

---
+++ m4-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/02-raspagem-de-dados/learning-exercises/02exceptions1.py
@@ -0,0 +1,8 @@
+import requests
+
+
+# À partir da décima requisição somos bloqueados de acessar o recurso
+# Código de status 429: Too Many Requests
+for _ in range(15):
+    response = requests.get("https://www.cloudflare.com/rate-limit-test/")
+    print(response.status_code)
---
+++ m4-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/02-raspagem-de-dados/learning-exercises/02exceptions2.py
@@ -0,0 +1,10 @@
+import requests
+import time
+
+
+# Coloca uma pausa de 6 segundos a cada requisição
+# Obs: este site de exemplo tem um rate limit de 10 requisições por minuto
+for _ in range(15):
+    response = requests.get("https://www.cloudflare.com/rate-limit-test/")
+    print(response)
+    time.sleep(6)
