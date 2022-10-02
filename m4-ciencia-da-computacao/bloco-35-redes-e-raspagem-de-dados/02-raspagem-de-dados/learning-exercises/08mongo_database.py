71161b0ec682a42542b207c81c6507f97ce4c4ce
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-02 19:50:49 -0300

    feat: 35.2 data scraping exercises

--- m4-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/02-raspagem-de-dados/learning-exercises/08mongo_database.py
+++ m4-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/02-raspagem-de-dados/learning-exercises/08mongo_database.py
@@ -1,6 +1,17 @@
 from pymongo import MongoClient
 
-# Por padrão o host é localhost e porta 27017
-# Estes valores podem ser modificados passando uma URL
-# client = MongoClient("mongodb://localhost:27017/")
 client = MongoClient()
+db = client.catalogue
+documents = [
+    {
+        "title": "A Light in the Attic",
+    },
+    {
+        "title": "Tipping the Velvet",
+    },
+    {
+        "title": "Soumission",
+    },
+]
+db.books.insert_many(documents)
+client.close()  # fecha a conexão com o banco de dados
