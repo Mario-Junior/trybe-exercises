fcadb37c116ce7f3f226bdad1b0a46f70b45bc1b
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-02 19:18:31 -0300

    feat: 35.2 data scraping exercises

---
+++ m4-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/02-raspagem-de-dados/learning-exercises/08mongo_database.py
@@ -0,0 +1,6 @@
+from pymongo import MongoClient
+
+# Por padrão o host é localhost e porta 27017
+# Estes valores podem ser modificados passando uma URL
+# client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient()
