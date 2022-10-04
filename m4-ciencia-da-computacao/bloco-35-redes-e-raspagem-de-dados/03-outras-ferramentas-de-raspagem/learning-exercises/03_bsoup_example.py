56c4a14e20d5c3218067f3644ad2bd974b32051c
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-04 10:26:06 -0300

    feat: 35.3 data scraping tools exercises

--- m4-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/03-outras-ferramentas-de-raspagem/learning-exercises/03_bsoup_example.py
+++ m4-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/03-outras-ferramentas-de-raspagem/learning-exercises/03_bsoup_example.py
@@ -9,8 +9,45 @@ html_content = page.text
 # do Python
 soup = BeautifulSoup(html_content, "html.parser")
 
-# acessando a tag 'title'
-title = soup.title
 
 # Utiliza o método prettify para melhorar a visualização do conteúdo
 print(soup.prettify())
+
+# acessando a tag 'title'
+title = soup.title
+
+# retorna o elemento HTML da tag
+print(title)
+
+# o tipo de 'title' é tag
+print(type(title))
+
+# o nome de 'title' é title
+print(title.name)
+
+# acessando a tag 'footer'
+footer = soup.footer
+
+# acessando o atributo classe da tag footer
+print(footer["class"])
+
+# Acessando a string de uma tag
+print(title.string)
+
+# Verificando o tipo dessa string
+print(type(title.string))
+
+# Imprime todas as ocorrências da tag "p" da página ou uma lista vazia,
+# caso nenhum elemento corresponda a pesquisa
+print(soup.find_all("p"))
+
+# Imprime o elemento com o id especificado ou "None",
+# caso nenhum elemento corresponda a pesquisa
+print(soup.find(id="quote"))
+
+# Imprime todo o texto da página
+print(soup.get_text())
+
+# Imprime todas as "divs" que possuam a classe "quote" ou uma lista vazia,
+# caso nenhum elemento corresponda a pesquisa
+print(soup.find_all("div", {"class": "quote"}))
