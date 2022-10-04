4a6658e2789e6edc0a93547b432d1479faf01e20
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-04 07:33:39 -0300

    feat: 35.3 data scraping tools exercises

---
+++ m4-ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/03-outras-ferramentas-de-raspagem/learning-exercises/02_scrape_selenium.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+
+# Importa o By
+from selenium.webdriver.common.by import By
+
+firefox = webdriver.Firefox()
+
+firefox.get("https://books.toscrape.com/")
+
+
+# Define a função que fará o scrape da URL recebida
+def scrape(url):
+    firefox.get(url)
+
+    # Itera entre os elementos com essa classe
+    for book in firefox.find_elements(By.CLASS_NAME, "product_pod"):
+        # Cria dict vazio para guardar os elementos capturados
+        new_item = {}
+
+        # Cria uma chave 'title' no dict que vai receber o resultado da busca
+        # O título está em uma tag anchor que está dentro de uma tag 'H3'
+        new_item["title"] = (
+            book.find_element(By.TAG_NAME, "h3")
+            .find_element(By.TAG_NAME, "a")
+            .get_attribute("innerHTML")
+        )
+
+        # O preço do book está em um elemento da classe 'entry-excerpt'
+        new_item["price"] = book.find_element(
+            By.CLASS_NAME, "price_color"
+        ).get_attribute("innerHTML")
+
+        # O link está dentro de um atributo 'href'
+        new_item["link"] = (
+            book.find_element(By.CLASS_NAME, "image_container")
+            .find_element(By.TAG_NAME, "a")
+            .get_attribute("href")
+        )
+
+        print(new_item)
+
+
+scrape("https://books.toscrape.com/")
