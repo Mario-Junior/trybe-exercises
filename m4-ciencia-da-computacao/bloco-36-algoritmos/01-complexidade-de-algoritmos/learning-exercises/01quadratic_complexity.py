868381b1d0583b4dbe122fec023d130f804d53a4
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-10 20:17:32 -0300

    feat: complexity of algorithms exercises

---
+++ m4-ciencia-da-computacao/bloco-36-algoritmos/01-complexidade-de-algoritmos/learning-exercises/01quadratic_complexity.py
@@ -0,0 +1,18 @@
+def multiply_arrays(array1, array2):
+    result = []
+    number_of_iterations = 0
+
+    for number1 in array1:
+        print(f'Array 1: {number1}')
+        for number2 in array2:
+            print(f'Array 2: {number2}')
+            result.append(number1 * number2)
+            number_of_iterations += 1
+
+    print(f'{number_of_iterations} iterações!')
+    return result
+
+
+meu_array = [1, 2, 3, 4, 5]
+
+multiply_arrays(meu_array, meu_array)
