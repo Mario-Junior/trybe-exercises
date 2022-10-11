3cfd85dc8e62211d2ad5438d483e2e731cbb8cab
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-10 21:16:40 -0300

    feat: complexity of algorithms exercises

---
+++ m4-ciencia-da-computacao/bloco-36-algoritmos/01-complexidade-de-algoritmos/learning-exercises/04.py
@@ -0,0 +1,27 @@
+def binary_search(numbers, target):
+    # definir os índices
+    start = 0
+    end = len(numbers) - 1
+
+    while start <= end: # os índices podem ser no máximo iguais, o início não pode ultrapassar o fim
+        mid = (start + end) // 2 # encontro o meio
+
+        if numbers[mid] == target: # se o elemento do meio for o alvo, devolve a posição do meio
+            return mid
+        
+        if target < numbers[mid]: # se o elemento for menor, atualiza o índice do fim
+            end = mid - 1
+        else: # caso contrário, atualiza o índice do inicio
+            start = mid + 1
+    
+    return -1 # Não encontrou? Retorna -1
+
+
+def logarithmic_complexity_two_arrays(array1, array2):
+    for item in array1:
+        binary_search(array2, item)
+
+array1 = [1, 2, 3, 4, 5]
+array2 = [2, 3, 4, 10, 40]
+
+print(logarithmic_complexity_two_arrays(array1, array2))
