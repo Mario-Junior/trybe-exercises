c93edaa0da14b02df497b5596b06f5749a76520e
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-23 11:23:09 -0300

    feat: data structure exercises

---
+++ m4-ciencia-da-computacao/bloco-37-estrutura-de-dados-arrays-listas-filas-pilhas/02-estrutura-de-dados-array/learning-exercises/numpy_array_example.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# define um array vazio de inteiros
+myarray = np.array([], dtype=int)
+
+# podemos adicionar alguns valores
+myarray = np.insert(myarray, 0, 5)  # na posição 0 o valor 5
+myarray = np.insert(myarray, 1, 3)
+myarray = np.insert(myarray, 2, 5)
+print("Após adicionar alguns valores: ", myarray)
+
+# adicionar em uma posição intermediária
+myarray = np.insert(myarray, 1, 4)
+print("Após inserção em índice intermediário: ", myarray)
+
+
+# remover um valor através do índice
+myarray = np.delete(myarray, 0)
+print("Após remover um valor:", myarray)
