5f5359c5b357320a41b25393c7bf26f3f1bde491
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-23 20:22:49 -0300

    feat: data structure exercises

---
+++ m4-ciencia-da-computacao/bloco-37-estrutura-de-dados-arrays-listas-filas-pilhas/03-nó-e-listas-encadeadas/learning-exercises/linked_list_content.py
@@ -0,0 +1,19 @@
+from node import Node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head_value = None
+        self.__length = 0
+
+    def __str__(self):
+        return f"LinkedList(len={self.__length}, value={self.head_value})"
+
+    def __len__(self):
+        return self.__length
+
+    def insert_first(self, value):
+        first_value = Node(value)
+        first_value.next = self.head_value
+        self.head_value = first_value
+        self.__length += 1
