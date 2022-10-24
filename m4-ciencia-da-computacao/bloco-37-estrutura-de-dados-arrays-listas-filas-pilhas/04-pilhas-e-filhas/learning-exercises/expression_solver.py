4a25dc74ed2039a038cd99aa96320f9d97f8debb
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-24 08:19:51 -0300

    feat: data structure exercises

--- m4-ciencia-da-computacao/bloco-37-estrutura-de-dados-arrays-listas-filas-pilhas/04-pilhas-e-filhas/learning-exercises/expression_solver.py
+++ m4-ciencia-da-computacao/bloco-37-estrutura-de-dados-arrays-listas-filas-pilhas/04-pilhas-e-filhas/learning-exercises/expression_solver.py
@@ -5,3 +5,11 @@ def solve_expression(expr):
     stack = Stack()
 
     tokens_list = expr.split(" ")
+    for token in tokens_list:
+        if token == "+":
+            # Sum operation
+            result = stack.pop() + stack.pop()
+            stack.push(result)
+        else:
+            # add number operation
+            stack.push(int(token))
