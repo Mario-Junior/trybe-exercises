4f3a47af2f50c0dd85266c9133b1cc44b4e81176
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-24 08:23:00 -0300

    feat: data structure exercises

--- m4-ciencia-da-computacao/bloco-37-estrutura-de-dados-arrays-listas-filas-pilhas/04-pilhas-e-filhas/learning-exercises/expression_solver.py
+++ m4-ciencia-da-computacao/bloco-37-estrutura-de-dados-arrays-listas-filas-pilhas/04-pilhas-e-filhas/learning-exercises/expression_solver.py
@@ -10,6 +10,15 @@ def solve_expression(expr):
             # Sum operation
             result = stack.pop() + stack.pop()
             stack.push(result)
+        elif token == '*':
+            # multiply operation
+            result = stack.pop() * stack.pop()
+            stack.push(result)
         else:
             # add number operation
             stack.push(int(token))
+
+    return stack.pop()
+
+
+print(solve_expression("5 10 + 3 *"))
