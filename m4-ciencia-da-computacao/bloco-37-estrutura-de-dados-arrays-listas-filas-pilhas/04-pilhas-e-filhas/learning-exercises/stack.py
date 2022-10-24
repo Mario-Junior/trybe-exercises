cf530c95e49fe25b4ae19a8816bff072819d8325
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-24 07:55:04 -0300

    feat: data structure exercises

---
+++ m4-ciencia-da-computacao/bloco-37-estrutura-de-dados-arrays-listas-filas-pilhas/04-pilhas-e-filhas/learning-exercises/stack.py
@@ -0,0 +1,9 @@
+class Stack:
+    def __init__(self):
+        self._data = list()
+
+    def size(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return not bool(self.size())
