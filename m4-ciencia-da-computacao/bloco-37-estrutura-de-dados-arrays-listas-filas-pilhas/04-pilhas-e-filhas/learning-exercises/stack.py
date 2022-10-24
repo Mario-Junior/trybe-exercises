6482a2e34db62ee788f3c5568ab0fb01cad7ecba
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-24 07:56:21 -0300

    feat: data structure exercises

--- m4-ciencia-da-computacao/bloco-37-estrutura-de-dados-arrays-listas-filas-pilhas/04-pilhas-e-filhas/learning-exercises/stack.py
+++ m4-ciencia-da-computacao/bloco-37-estrutura-de-dados-arrays-listas-filas-pilhas/04-pilhas-e-filhas/learning-exercises/stack.py
@@ -7,3 +7,16 @@ class Stack:
 
     def is_empty(self):
         return not bool(self.size())
+
+    def push(self, value):
+        self._data.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+
+        # -1 se refere ao último objeto da pilha.
+        # Ou seja, o valor do topo da pilha
+        value = self._data[-1]
+        del self._data[-1]
+        return value
