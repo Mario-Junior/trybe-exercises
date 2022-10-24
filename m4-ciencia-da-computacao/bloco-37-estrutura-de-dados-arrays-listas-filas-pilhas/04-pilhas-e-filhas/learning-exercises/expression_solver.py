0be4d8d177e84d738f99f89c90cf5c64398d083f
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-24 08:23:53 -0300

    feat: data structure exercises

--- m4-ciencia-da-computacao/bloco-37-estrutura-de-dados-arrays-listas-filas-pilhas/04-pilhas-e-filhas/learning-exercises/expression_solver.py
+++ m4-ciencia-da-computacao/bloco-37-estrutura-de-dados-arrays-listas-filas-pilhas/04-pilhas-e-filhas/learning-exercises/expression_solver.py
@@ -3,14 +3,14 @@ from stack import Stack
 
 def solve_expression(expr):
     stack = Stack()
-
     tokens_list = expr.split(" ")
+
     for token in tokens_list:
         if token == "+":
             # Sum operation
             result = stack.pop() + stack.pop()
             stack.push(result)
-        elif token == '*':
+        elif token == "*":
             # multiply operation
             result = stack.pop() * stack.pop()
             stack.push(result)
