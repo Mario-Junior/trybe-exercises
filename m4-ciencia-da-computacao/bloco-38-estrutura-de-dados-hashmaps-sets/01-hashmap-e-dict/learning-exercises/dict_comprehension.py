42d2d27c0d1541d408b7d7a68029243458e0dd0f
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-29 16:26:29 -0300

    feat: data structure exercises

--- m4-ciencia-da-computacao/bloco-38-estrutura-de-dados-hashmaps-sets/01-hashmap-e-dict/learning-exercises/dict_comprehension.py
+++ m4-ciencia-da-computacao/bloco-38-estrutura-de-dados-hashmaps-sets/01-hashmap-e-dict/learning-exercises/dict_comprehension.py
@@ -2,4 +2,14 @@
 # que mapeia inteiros de 3 a 20 para o seu dobro.
 
 double = {i: i*2 for i in range(3, 21)}
-print(double)
+print(f"Todos duplicados: {double}")
+
+# Exercício 7: Utilizando o dicionário criado no exercício 5.
+# Para as chaves ímpares, mapear o triplo.
+# Consulte o método keys() e atualize o dicionário para a nova regra.
+
+for key in double.keys():
+    if key % 2 != 0:
+        double[key] = key * 3
+
+print(f"Ímpares triplicados: {double}")
