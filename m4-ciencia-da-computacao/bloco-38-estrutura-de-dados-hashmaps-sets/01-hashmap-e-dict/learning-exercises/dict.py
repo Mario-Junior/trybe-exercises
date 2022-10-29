729e886369bd34ef0a96358301d295f85026b0b7
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-29 16:16:41 -0300

    feat: data structure exercises

--- m4-ciencia-da-computacao/bloco-38-estrutura-de-dados-hashmaps-sets/01-hashmap-e-dict/learning-exercises/dict.py
+++ m4-ciencia-da-computacao/bloco-38-estrutura-de-dados-hashmaps-sets/01-hashmap-e-dict/learning-exercises/dict.py
@@ -13,3 +13,26 @@ print(employee_registry)
 # objeto[chave] = novo_valor
 employee_registry[10] = "name30"
 print(f"Novo valor do id 10, após a atualização: {employee_registry[10]}")
+
+
+# Instanciação do objeto vazio
+dict1 = {}
+dict2 = dict()
+
+# Instanciação com preenchimento inicial de dados
+dict3 = {1: 'name1', 2: 'name2'}
+print(f"Dicionário 1: {dict1}")
+print(f"Dicionário 2: {dict2}")
+print(f"Dicionário 3: {dict3}")
+
+# Inserção e Alteração
+# Se a chave não existir no dict, uma nova entrada será criada
+# Se já existir, o valor será sobreposto
+dict1[14] = 'name1'
+print(f"Novo dicionário 1, pós inserção/alteração: {dict1}")
+
+# Consulta e Remoção
+# Se a chave não existir no dict, causa erro
+name = dict1[14]
+del dict1[14]
+print(f"Dicionário 1 pós consulta e deleção: {dict1}")
---
+++ m4-ciencia-da-computacao/bloco-38-estrutura-de-dados-hashmaps-sets/01-hashmap-e-dict/learning-exercises/dict_comprehension.py
@@ -0,0 +1,5 @@
+# Exercício 5: Criar dicionário chamado 'dict comprehension'
+# que mapeia inteiros de 3 a 20 para o seu dobro.
+
+double = {i: i*2 for i in range(3, 21)}
+print(double)
