cff7e120c62aa280c4ca7e140e3a3e20cec83e49
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-29 20:19:54 -0300

    feat: data structure exercises

--- m4-ciencia-da-computacao/bloco-38-estrutura-de-dados-hashmaps-sets/02-set/learning-exercises/set.py
+++ m4-ciencia-da-computacao/bloco-38-estrutura-de-dados-hashmaps-sets/02-set/learning-exercises/set.py
@@ -3,6 +3,18 @@ class Conjunto:
         self.set = [False] * 1001
         self.last_element = 0
 
+    def __str__(self):
+        string = "{"
+
+        for index, is_in_set in enumerate(self.set):
+            if is_in_set:
+                string += str(index)
+                if index < self.last_element:
+                    string += ", "
+
+        string += "}"
+        return string
+
     def add(self, item):
         if not self.set[item]:
             self.set[item] = True
@@ -12,6 +24,19 @@ class Conjunto:
 
 if __name__ == "__main__":
     conj = Conjunto()
+    for i in [0, 10, 100, 1000]:
+        conj.add(i)
+    print(conj)
 
-    for item in [0, 10, 100, 1000]:
-        conj.add(item)
+    conj2 = Conjunto()
+    for i in [1, 2, 3]:
+        conj2.add(i)
+    print(conj2)
+
+    conj3 = Conjunto()
+    for i in [7, 2, 10]:
+        conj3.add(i)
+    print(conj3)
+
+    conj4 = Conjunto()
+    print(conj4)
