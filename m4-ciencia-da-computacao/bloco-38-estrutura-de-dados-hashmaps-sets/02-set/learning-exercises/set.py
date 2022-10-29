bc44ebfc65a0dc342f5126e762f40cf7fd5576e5
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-29 20:28:55 -0300

    feat: data structure exercises

--- m4-ciencia-da-computacao/bloco-38-estrutura-de-dados-hashmaps-sets/02-set/learning-exercises/set.py
+++ m4-ciencia-da-computacao/bloco-38-estrutura-de-dados-hashmaps-sets/02-set/learning-exercises/set.py
@@ -24,8 +24,18 @@ class Conjunto:
     def __contains__(self, item):
         return self.set[item]
 
+    def union(self, conjuntoB):
+        new_conjunto = Conjunto()
+
+        for index in range(1001):
+            if self.set[index] or conjuntoB.set[index]:
+                new_conjunto.add(index)
+
+        return new_conjunto
+
 
 if __name__ == "__main__":
+    # Inicialização e Adição
     conj = Conjunto()
     for i in [0, 10, 100, 1000]:
         conj.add(i)
@@ -44,9 +54,24 @@ if __name__ == "__main__":
     conj4 = Conjunto()
     print(conj4)
 
-    conj5 = Conjunto()
+    # Contido (ou não contido)
+    conj = Conjunto()
     for i in [1, 2, 3]:
-        conj5.add(i)
-    print(conj5)
-    print(1 in conj5)
-    print(0 in conj5)
+        conj.add(i)
+    print(conj)
+    print(1 in conj)
+    print(0 in conj)
+
+    # União
+    conj1 = Conjunto()
+    for i in range(1, 11):
+        conj1.add(i)
+
+    conj2 = Conjunto()
+    for i in range(10, 21):
+        conj2.add(i)
+
+    conj3 = conj1.union(conj2)
+    print(f"Conjunto 1: {conj1}")
+    print(f"Conjunto 2: {conj2}")
+    print(f"1 U 2: {conj3}")
